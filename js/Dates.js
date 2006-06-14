e6766009bbdbf16214739b2fd1839f0ead294ab9
JeremyRuston <JeremyRuston@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2006-06-14 16:30:30 +0000

    Added date format support for "hh12" and "0hh12" for 12-hour clock, and "am"/"AM" for am/pm indicator

--- js/Dates.js
+++ js/Dates.js
@@ -16,13 +16,28 @@ Date.prototype.formatString = function(template)
 	template = template.replace(/0DD/g,String.zeroPad(this.getDate(),2));
 	template = template.replace(/DDth/g,this.getDate()+this.daySuffix());
 	template = template.replace(/DD/g,this.getDate());
+	template = template.replace(/0hh12/g,String.zeroPad(this.getHours12(),2));
+	template = template.replace(/hh12/g,this.getHours12());
 	template = template.replace(/0hh/g,String.zeroPad(this.getHours(),2));
 	template = template.replace(/hh/g,this.getHours());
 	template = template.replace(/0mm/g,String.zeroPad(this.getMinutes(),2));
 	template = template.replace(/mm/g,this.getMinutes());
 	template = template.replace(/0ss/g,String.zeroPad(this.getSeconds(),2));
 	template = template.replace(/ss/g,this.getSeconds());
+	template = template.replace(/[ap]m/g,this.getAmPm().toLowerCase());
+	template = template.replace(/[AP]M/g,this.getAmPm().toUpperCase());
 	return template;
+}
+
+Date.prototype.getHours12 = function()
+{
+	var h = this.getHours();
+	return h > 12 ? h-12 : ( h > 0 ? h : 12 );
+}
+
+Date.prototype.getAmPm = function()
+{
+	return this.getHours() >= 12 ? "pm" : "am";
 }
 
 Date.prototype.daySuffix = function()
