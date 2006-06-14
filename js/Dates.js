fed99944a62173c5250ddb35b31018fcb79f1c2f
JeremyRuston <JeremyRuston@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2006-06-14 16:33:15 +0000

    Added support for ISO-8601 week numbers (ticket #60)

--- js/Dates.js
+++ js/Dates.js
@@ -5,12 +5,16 @@
 // Substitute date components into a string
 Date.prototype.formatString = function(template)
 {
+	template = template.replace(/wYYYY/g,this.getYearForWeekNo());
+	template = template.replace(/wYY/g,String.zeroPad(this.getYearForWeekNo()-2000,2));
 	template = template.replace(/YYYY/g,this.getFullYear());
 	template = template.replace(/YY/g,String.zeroPad(this.getFullYear()-2000,2));
 	template = template.replace(/MMM/g,config.messages.dates.months[this.getMonth()]);
 	template = template.replace(/mmm/g,config.messages.dates.shortMonths[this.getMonth()]);
 	template = template.replace(/0MM/g,String.zeroPad(this.getMonth()+1,2));
 	template = template.replace(/MM/g,this.getMonth()+1);
+	template = template.replace(/0WW/g,String.zeroPad(this.getWeek(),2));
+	template = template.replace(/WW/g,this.getWeek());
 	template = template.replace(/DDD/g,config.messages.dates.days[this.getDay()]);
 	template = template.replace(/ddd/g,config.messages.dates.shortDays[this.getDay()]);
 	template = template.replace(/0DD/g,String.zeroPad(this.getDate(),2));
@@ -27,6 +31,25 @@ Date.prototype.formatString = function(template)
 	template = template.replace(/[ap]m/g,this.getAmPm().toLowerCase());
 	template = template.replace(/[AP]M/g,this.getAmPm().toUpperCase());
 	return template;
+}
+
+Date.prototype.getWeek = function()
+{
+	var dt = new Date(this.getTime());
+	var d = dt.getDay();
+	if (d==0) d=7;// JavaScript Sun=0, ISO Sun=7
+	dt.setTime(dt.getTime()+(4-d)*86400000);// shift day to Thurs of same week to calculate weekNo
+	var n = Math.floor((dt.getTime()-new Date(dt.getFullYear(),0,1)+3600000)/86400000); 
+	return Math.floor(n/7)+1;
+}
+
+Date.prototype.getYearForWeekNo = function()
+{
+	var dt = new Date(this.getTime());
+	var d = dt.getDay();
+	if (d==0) d=7;// JavaScript Sun=0, ISO Sun=7
+	dt.setTime(dt.getTime()+(4-d)*86400000);// shift day to Thurs of same week
+	return dt.getFullYear();
 }
 
 Date.prototype.getHours12 = function()
