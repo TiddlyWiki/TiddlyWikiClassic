2dc14305b82aebfe7aea9835818b11a50cbf73aa
JeremyRuston <JeremyRuston@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2006-06-30 16:56:52 +0000

    Refactoring prettyLink for better extendability (ticket #24)

--- js/FormatterHelpers.js
+++ js/FormatterHelpers.js
@@ -101,7 +101,24 @@ config.formatterHelpers = {
 
 	isExternalLink: function(link)
 	{
-		return !(store.tiddlerExists(link) || store.isShadowTiddler(link));
+		if(store.tiddlerExists(link) || store.isShadowTiddler(link))
+			{
+			// Definitely not an external link
+			return false;
+			}
+		var urlRegExp = new RegExp(config.textPrimitives.urlPattern,"mg");
+		if(urlRegExp.exec(link))
+			{
+			// Definitely an external link
+			return true;
+			}
+		if (link.indexOf(".")!=-1 || link.indexOf("\\")!=-1 || link.indexOf("/")!=-1)
+			{
+			// Link contains . / or \ so is probably an external link
+			return true;
+			}
+		// Otherwise assume it is not an external link
+		return false;
 	}
 
 };
