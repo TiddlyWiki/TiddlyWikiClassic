8b3a3a0120a1ffde57c5c0b00c353c6aa9cba074
JeremyRuston <JeremyRuston@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2006-07-11 10:16:48 +0000

    Refactoring to use String.unescapeLineBreaks (instead of Tiddler.unescapeLineBreaks)

--- js/TW21Loader.js
+++ js/TW21Loader.js
@@ -9,7 +9,7 @@ TW21Loader.getMetaDataFromAttributes = function(tiddlerDiv) {
 	for(var i = attrs.length-1; i >= 0; i--) {
 		var name = attrs[i].name;
 		if (!TiddlyWiki.isLegacyField(name)) {
-			result[name] = Tiddler.unescapeLineBreaks(attrs[i].value);
+			result[name] = attrs[i].value.unescapeLineBreaks();
 		}
 	}
 	return result;
