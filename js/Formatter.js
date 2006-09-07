5d17396e90eeb34c4448f5b830fef9dc738d577b
JeremyRuston <JeremyRuston@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2006-09-07 14:52:05 +0000

    Extend PrettyLink format (ticket #160)

--- js/Formatter.js
+++ js/Formatter.js
@@ -353,7 +353,13 @@ config.formatters = [
 			else if(lookaheadMatch[3]) // Pretty bracketted link
 				{
 				var link = lookaheadMatch[4];
-				e = config.formatterHelpers.isExternalLink(link) ? createExternalLink(w.output,link) : createTiddlyLink(w.output,link,false,null,w.isStatic);
+				var i = link.indexOf("|");
+				if (i) 
+					{
+					var s = link.substr(0,i);
+					link = (store.getTiddler(s) || store.isShadowTiddler(s)) ? s : link.substr(i+1);
+					}
+				e = config.formatterHelpers.isExternalLink(link) ? createExternalLink(w.output,link) : createTiddlyLink(w.output,link,false);
 				}
 			createTiddlyText(e,text);
 			w.nextMatch = this.lookaheadRegExp.lastIndex;
