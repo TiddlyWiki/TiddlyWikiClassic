6f12c78bad27ee8ce6abf424eea23a481002030f
JeremyRuston <JeremyRuston@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2006-12-07 15:27:13 +0000

    Added comment pointing to full subversion repository

---
+++ js/Guide.js
@@ -0,0 +1,8 @@
+//
+// 
+// This code is designed to be readable but for compactness it only includes brief comments. You can see fuller comments
+// in the project Subversion repository:
+//
+// http://svn.tiddlywiki.org/Trunk/core/
+//
+//
