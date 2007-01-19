adf16a742ff643180b4888deaa340b80fdd32f6a
JeremyRuston <JeremyRuston@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-01-19 09:56:59 +0000

    Updated guide code comments

--- js/Guide.js
+++ js/Guide.js
@@ -1,8 +1,9 @@
 //
+// Please note:
 // 
-// This code is designed to be readable but for compactness it only includes brief comments. You can see fuller comments
-// in the project Subversion repository:
+// * This code is designed to be readable but for compactness it only includes brief comments. You can see fuller comments
+//   in the project Subversion repository at http://svn.tiddlywiki.org/Trunk/core/
 //
-// http://svn.tiddlywiki.org/Trunk/core/
-//
-//
+// * You should never need to modify this source code directly. TiddlyWiki is carefully designed to allow deep customisation
+//   without changing the core code. Please consult the development group at http://groups.google.com/group/TiddlyWikiDev
+// 
