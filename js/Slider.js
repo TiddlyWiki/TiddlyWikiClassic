b01da0fc256b45006c8fb3e1b75ea400fc4f5df7
JeremyRuston <JeremyRuston@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-02-04 22:32:19 +0000

    Upaded to K&R code layout (ticket #232)

--- js/Slider.js
+++ js/Slider.js
@@ -1,6 +1,6 @@
-// ---------------------------------------------------------------------------------
-// Slider animation
-// ---------------------------------------------------------------------------------
+//--
+//-- Slider animation
+//--
 
 // deleteMode - "none", "all" [delete target element and it's children], [only] "children" [but not the target element]
 function Slider(element,opening,slowly,deleteMode)
@@ -12,33 +12,26 @@ function Slider(element,opening,slowly,deleteMode)
 	this.realHeight = element.offsetHeight;
 	this.opening = opening;
 	this.step = slowly ? config.animSlow : config.animFast;
-	if(opening)
-		{
+	if(opening) {
 		this.progress = 0;
 		element.style.height = "0px";
 		element.style.display = "block";
-		}
-	else
-		{
+	} else {
 		this.progress = 1;
 		this.step = -this.step;
-		}
+	}
 	element.style.overflow = "hidden";
 	return this;
 }
 
 Slider.prototype.stop = function()
 {
-	if(this.opening)
-		{
+	if(this.opening) {
 		this.element.style.height = "auto";
 		this.element.style.opacity = 1;
 		this.element.style.filter = "alpha(opacity:100)";
-		}
-	else
-		{
-		switch(this.deleteMode)
-			{
+	} else {
+		switch(this.deleteMode) {
 			case "none":
 				this.element.style.display = "none";
 				this.element.style.opacity = 1;
@@ -50,26 +43,23 @@ Slider.prototype.stop = function()
 			case "children":
 				removeChildren(this.element);
 				break;
-			}
 		}
-}
+	}
+};
 
 Slider.prototype.tick = function()
 {
 	this.progress += this.step;
-	if(this.progress < 0 || this.progress > 1)
-		{
+	if(this.progress < 0 || this.progress > 1) {
 		this.stop();
 		return false;
-		}
-	else
-		{
+	} else {
 		var f = Animator.slowInSlowOut(this.progress);
 		var h = this.realHeight * f;
 		this.element.style.height = h + "px";
 		this.element.style.opacity = f;
 		this.element.style.filter = "alpha(opacity:" + f * 100 +")";
 		return true;
-		}
-}
+	}
+};
 
