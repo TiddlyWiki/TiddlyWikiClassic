2e21ac922def8099588bb6e4fe145ead639081d3
JeremyRuston <JeremyRuston@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-05-06 19:51:10 +0000

    Removed extraneous console.log() calls

--- js/Dom.js
+++ js/Dom.js
@@ -305,7 +305,6 @@ function replaceSelection(e,text)
 {
 	if(e.setSelectionRange) {
 		var oldpos = e.selectionStart;
-		console.log(e.selectionEnd);console.log(e.selectionStart);
 		var isRange = e.selectionEnd > e.selectionStart;
 		e.value = e.value.substr(0,e.selectionStart) + text + e.value.substr(e.selectionEnd);
 		e.setSelectionRange(isRange ? oldpos : oldpos + text.length,oldpos + text.length);
