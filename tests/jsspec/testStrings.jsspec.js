7039ff7431c24ae5c9206115f3a8d0831c15e25b
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-10 16:40:58 +0000

    More tests

---
+++ tests/jsspec/testStrings.jsspec.js
@@ -0,0 +1,19 @@
+// <![CDATA[
+describe('Strings', {
+	'String right': function() {
+		var actual = "abcdef".right(3);
+		var expected = "def";		
+		value_of(actual).should_be(expected);
+	},
+	'String trim': function() {
+		var actual = " abcdef ".trim();
+		var expected = "abcdef";		
+		value_of(actual).should_be(expected);
+	},
+	'String undash': function() {
+		var actual = "background-color".unDash();
+		var expected = "backgroundColor";		
+		value_of(actual).should_be(expected);
+	}
+})
+// ]]>
---
+++ tests/jsspec/testTiddler.jsspec.js
@@ -0,0 +1,17 @@
+// <![CDATA[
+describe('Tiddlers', {
+	'Tiddler changecount': function() {
+		var tiddler = new Tiddler("temp");
+		value_of(tiddler.isTouched()).should_be(false);
+		tiddler.incChangeCount();
+		value_of(tiddler.isTouched()).should_be(true);
+		tiddler.clearChangeCount();
+		value_of(tiddler.isTouched()).should_be(false);
+	},
+	'Tiddler tags': function() {
+		var tiddler = new Tiddler("temp");
+		value_of(tiddler.isTagged()).should_be(false);
+		value_of(tiddler.getTags()).should_be("");
+	},
+})
+// ]]>
