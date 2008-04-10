e3f7fcb7740a366582c1cf100d043b49c3eb52e6
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-10 16:15:54 +0000

    Added more tests

--- tests/jsspec/testWikifier.jsspec.js
+++ tests/jsspec/testWikifier.jsspec.js
@@ -8,9 +8,28 @@ describe('Wikifier', {
 	},
 	'Italic formatting': function() {
 		formatter = new Formatter(config.formatters);
-		var actual = wikifyStatic("//italic");
+		var actual = wikifyStatic("//italic//");
 		var expected = "<em>italic</em>";		
 		value_of(actual).should_be(expected);
-	}
+	},
+	'Underline formatting': function() {
+		formatter = new Formatter(config.formatters);
+		var actual = wikifyStatic("__underline__");
+		var expected = "<u>underline</u>";		
+		value_of(actual).should_be(expected);
+	},
+	'Superscript formatting': function() {
+		formatter = new Formatter(config.formatters);
+		var actual = wikifyStatic("^^superscript^^");
+		var expected = "<sup>superscript</sup>";		
+		value_of(actual).should_be(expected);
+	},
+	'Subscript formatting': function() {
+		formatter = new Formatter(config.formatters);
+		var actual = wikifyStatic("~~subscript~~");
+		var expected = "<sub>subscript</sub>";		
+		value_of(actual).should_be(expected);
+	},
 })
 // ]]>
+
