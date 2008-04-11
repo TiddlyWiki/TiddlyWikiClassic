e4bad70439ca3c3564badfeb22b4f6ac18425f37
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-11 09:49:09 +0000

    Additional tests

--- tests/jsspec/testDates.jsspec.js
+++ tests/jsspec/testDates.jsspec.js
@@ -5,6 +5,18 @@ describe('Dates', {
 		var expected = "2007 March 1";
 		
 		value_of(actual).should_be(expected);
-	}
+	},
+	'Date formatting YYYY MMM DD hh:mm ss': function() {
+		var actual = new Date(2008,11,31,23,48,59).formatString("YYYY MMM DD hh:mm ss");
+		var expected = "2008 December 31 23:48 59";
+		
+		value_of(actual).should_be(expected);
+	},
+	'Date convertFromYYYYMMDDHHMM': function() {
+		var actual = Date.convertFromYYYYMMDDHHMM("200812312348");
+		var expected = new Date(2008,11,31,23,48);
+		
+		value_of(actual).should_be(expected);
+	},
 })
 // ]]>
