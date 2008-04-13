a5577134ff5360962e1c30b1568a909717cd8678
JeremyRuston <JeremyRuston@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-13 17:16:32 +0000

    Setting version number for beta 1 of 2.4.0

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 3, revision: 0, date: new Date("Dec 4, 2007"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 4, revision: 0, beta: 1, date: new Date("Apr 13, 2008"), extensions: {}};
