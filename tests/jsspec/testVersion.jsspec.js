f46e8c67e901564fa7690ff21cc660ded8f25359
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-14 12:03:30 +0000

    tests: added some core Version tests

---
+++ tests/jsspec/testVersion.jsspec.js
@@ -0,0 +1,30 @@
+// <![CDATA[
+describe('Version', {
+	'should contain a version string': function() {	
+		value_of(version).should_not_be_null();
+	},
+	'the title should be "TiddlyWiki"': function() {	
+		value_of(version.title).should_be('TiddlyWiki');
+	},
+	'the major value should be a number': function() {	
+		value_of(typeof version.major).should_be('number');
+	},
+	'the minor value should be a number': function() {	
+		value_of(typeof version.minor).should_be('number');
+	},
+	'the revision value should be a number': function() {	
+		value_of(typeof version.revision).should_be('number');
+	},
+	'the beta value should be a revision number': function() {	
+		value_of(typeof version.beta).should_be('number');
+	},
+	'the date value should be a Date': function() {	
+		value_of(typeof version.date).should_be('object');
+		value_of(version.date.convertToYYYYMMDDHHMM()).should_match(/^\d{12,12}$/);
+	},
+	'the extensions value should be an object': function() {	
+		value_of(typeof version.extensions).should_be('object');
+	},
+});
+// ]]>
+
