71e32ba58cea63ae78e442695465c64de6b486fa
jayfresh <jayfresh@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-14 11:18:46 +0000

    testStrings: added JSSpec tests to test String.prototype.format

--- tests/jsspec/testStrings.jsspec.js
+++ tests/jsspec/testStrings.jsspec.js
@@ -14,6 +14,31 @@ describe('Strings', {
 		var actual = "background-color".unDash();
 		var expected = "backgroundColor";		
 		value_of(actual).should_be(expected);
+	},
+	'String format with an empty substring array should return input string': function() {
+		var actual = "hello %0, is your favourite colour red?".format([]);
+		var expected = "hello , is your favourite colour red?";
+		value_of(actual).should_be(expected);
+	},
+	'String format with a substrings array of correct size (1) should add substrings in the right places': function() {
+		var actual = "hello %0, is your favourite colour red?".format(["Jon"]);
+		var expected = "hello Jon, is your favourite colour red?";
+		value_of(actual).should_be(expected);
+	},
+	'String format with a substrings array of more than enough substrings (1 needed) should add substrings in the right places': function() {
+		var actual = "hello %0, is your favourite colour red?".format(["Jon","rhubarb","rhubarb"]);
+		var expected = "hello Jon, is your favourite colour red?";
+		value_of(actual).should_be(expected);
+	},
+	'String format with an empty substring array and no %1-type specifiers should return input string': function() {
+		var actual = "hello Jon, is your favourite colour red?".format([]);
+		var expected = "hello Jon, is your favourite colour red?";
+		value_of(actual).should_be(expected);
+	},
+	'String format with a substrings array of non-zero size (1) and no %1-type specifiers should return input string': function() {
+		var actual = "hello Jon, is your favourite colour red?".format(["rhubarb"]);
+		var expected = "hello Jon, is your favourite colour red?";
+		value_of(actual).should_be(expected);
 	}
 });
 // ]]>
