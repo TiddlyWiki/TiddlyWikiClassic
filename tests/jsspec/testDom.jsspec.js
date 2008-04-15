293b6fa11c063dda94cbb56c5be9d2cb50972290
jayfresh <jayfresh@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-15 11:03:25 +0000

    TiddlyWiki core test: refined tests for createTiddlyElement

--- tests/jsspec/testDom.jsspec.js
+++ tests/jsspec/testDom.jsspec.js
@@ -1,15 +1,22 @@
 // <![CDATA[
-describe('TiddlyWiki DOM functions', {
-	'Element creation': function() {
+describe('TiddlyWiki DOM element creation', {
+	'Element creation should create the DOM element': function() {
 		var actual = createTiddlyElement(null,"div");
 		value_of(actual).should_not_be_null();
 	},
-	'Element id': function() {
+	'Setting the parent parameter should append the new DOM element to the parent': function() {
+		var parent = document.body;
+		var before = document.body.childNodes.length;
+		var actual = createTiddlyElement(parent,"div");
+		var after = document.body.childNodes.length;
+		value_of(++before).should_be(after);
+	},
+	'Setting the element id parameter should set the id on the DOM element': function() {
 		var actual = createTiddlyElement(null,"div","testId").id;
 		var expected = "testId";		
 		value_of(actual).should_be(expected);
 	},
-	'Element class': function() {
+	'Setting the class parameter should set the class on the DOM element': function() {
 		var actual = createTiddlyElement(null,"div",null,"testClass").className;
 		var expected = "testClass";		
 		value_of(actual).should_be(expected);
