b0164f4408fad82a4c7dd34151179c8979c2f3c7
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-15 11:01:50 +0000

    tests: date components; short- and full-month format

--- tests/jsspec/testDates.jsspec.js
+++ tests/jsspec/testDates.jsspec.js
@@ -39,6 +39,126 @@ describe('Date components', {
 		var actual = new Date(2008,11,31,21,48).formatString("hh");
 		var expected = "21";
 		value_of(actual).should_be(expected);
+	},
+	'short-month format should return "Jan" for month 0': function() {
+		var actual = new Date(2008,0).formatString("mmm");
+		var expected = "Jan";
+		value_of(actual).should_be(expected);
+	},
+	'short-month format should return "Feb" for month 1': function() {
+		var actual = new Date(2008,1).formatString("mmm");
+		var expected = "Feb";
+		value_of(actual).should_be(expected);
+	},
+	'short-month format should return "Mar" for month 2': function() {
+		var actual = new Date(2008,2).formatString("mmm");
+		var expected = "Mar";
+		value_of(actual).should_be(expected);
+	},
+	'short-month format should return "Apr" for month 3': function() {
+		var actual = new Date(2008,3).formatString("mmm");
+		var expected = "Apr";
+		value_of(actual).should_be(expected);
+	},
+	'short-month format should return "May" for month 4': function() {
+		var actual = new Date(2008,4).formatString("mmm");
+		var expected = "May";
+		value_of(actual).should_be(expected);
+	},
+	'short-month format should return "Jun" for month 5': function() {
+		var actual = new Date(2008,5).formatString("mmm");
+		var expected = "Jun";
+		value_of(actual).should_be(expected);
+	},
+	'short-month format should return "Jul" for month 6': function() {
+		var actual = new Date(2008,6).formatString("mmm");
+		var expected = "Jul";
+		value_of(actual).should_be(expected);
+	},
+	'short-month format should return "Aug" for month 7': function() {
+		var actual = new Date(2008,7).formatString("mmm");
+		var expected = "Aug";
+		value_of(actual).should_be(expected);
+	},
+	'short-month format should return "Sep" for month 8': function() {
+		var actual = new Date(2008,8).formatString("mmm");
+		var expected = "Sep";
+		value_of(actual).should_be(expected);
+	},
+	'short-month format should return "Oct" for month 9': function() {
+		var actual = new Date(2008,9).formatString("mmm");
+		var expected = "Oct";
+		value_of(actual).should_be(expected);
+	},
+	'short-month format should return "Nov" for month 10': function() {
+		var actual = new Date(2008,10).formatString("mmm");
+		var expected = "Nov";
+		value_of(actual).should_be(expected);
+	},
+	'short-month format should return "Dec" for month 12': function() {
+		var actual = new Date(2008,11).formatString("mmm");
+		var expected = "Dec";
+		value_of(actual).should_be(expected);
+	},
+	'full-month format should return "January" for month 0': function() {
+		var actual = new Date(2008,0).formatString("MMM");
+		var expected = "January";
+		value_of(actual).should_be(expected);
+	},
+	'full-month format should return "February" for month 1': function() {
+		var actual = new Date(2008,1).formatString("MMM");
+		var expected = "February";
+		value_of(actual).should_be(expected);
+	},
+	'full-month format should return "March" for month 2': function() {
+		var actual = new Date(2008,2).formatString("MMM");
+		var expected = "March";
+		value_of(actual).should_be(expected);
+	},
+	'full-month format should return "April" for month 3': function() {
+		var actual = new Date(2008,3).formatString("MMM");
+		var expected = "April";
+		value_of(actual).should_be(expected);
+	},
+	'full-month format should return "May" for month 4': function() {
+		var actual = new Date(2008,4).formatString("MMM");
+		var expected = "May";
+		value_of(actual).should_be(expected);
+	},
+	'full-month format should return "June" for month 5': function() {
+		var actual = new Date(2008,5).formatString("MMM");
+		var expected = "June";
+		value_of(actual).should_be(expected);
+	},
+	'full-month format should return "July" for month 6': function() {
+		var actual = new Date(2008,6).formatString("MMM");
+		var expected = "July";
+		value_of(actual).should_be(expected);
+	},
+	'full-month format should return "August" for month 7': function() {
+		var actual = new Date(2008,7).formatString("MMM");
+		var expected = "August";
+		value_of(actual).should_be(expected);
+	},
+	'full-month format should return "September" for month 8': function() {
+		var actual = new Date(2008,8).formatString("MMM");
+		var expected = "September";
+		value_of(actual).should_be(expected);
+	},
+	'full-month format should return "October" for month 9': function() {
+		var actual = new Date(2008,9).formatString("MMM");
+		var expected = "October";
+		value_of(actual).should_be(expected);
+	},
+	'full-month format should return "November" for month 10': function() {
+		var actual = new Date(2008,10).formatString("MMM");
+		var expected = "November";
+		value_of(actual).should_be(expected);
+	},
+	'full-month format should return "December" for month 12': function() {
+		var actual = new Date(2008,11).formatString("MMM");
+		var expected = "December";
+		value_of(actual).should_be(expected);
 	}
 });
 
