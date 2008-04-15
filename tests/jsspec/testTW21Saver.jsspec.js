6eaa14e4c8ba0ae91e8c74f3d5cc9f251c18a91e
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-15 13:14:36 +0000

    More tests

--- tests/jsspec/testTW21Saver.jsspec.js
+++ tests/jsspec/testTW21Saver.jsspec.js
@@ -5,25 +5,47 @@ describe('TW21Saver', {
 		saver = store.getSaver();
 	},
 
-	'Saving empty tiddler': function() {
+	'Saving empty tiddler with no dates': function() {
 		store = new TiddlyWiki();
 		tiddler = new Tiddler("test");
 		tiddler.text = "";
-		tiddler.created = new Date(Date.UTC(2008,3,16));
+		tiddler.created = version.date;
 		tiddler.modified = tiddler.created;
 
 		var actual = saver.externalizeTiddler(store,tiddler);
-		var expected = "<div title=\"test\" created=\"200804160000\">\n<pre></pre>\n</div>";
+		var expected = "<div title=\"test\">\n<pre></pre>\n</div>";
+		value_of(actual).should_be(expected);
+	},
+	'Saving empty tiddler with created date': function() {
+		store = new TiddlyWiki();
+		tiddler = new Tiddler("test");
+		tiddler.text = "";
+		tiddler.created = new Date(Date.UTC(2008,3,16,12,34));
+		tiddler.modified = tiddler.created;
+
+		var actual = saver.externalizeTiddler(store,tiddler);
+		var expected = "<div title=\"test\" created=\"200804161234\">\n<pre></pre>\n</div>";
+		value_of(actual).should_be(expected);
+	},
+	'Saving empty tiddler with created and modified dates': function() {
+		store = new TiddlyWiki();
+		tiddler = new Tiddler("test");
+		tiddler.text = "";
+		tiddler.created = new Date(Date.UTC(2008,3,16,12,34));
+		tiddler.modified = new Date(Date.UTC(2008,3,17,12,34));
+
+		var actual = saver.externalizeTiddler(store,tiddler);
+		var expected = "<div title=\"test\" modified=\"200804171234\" created=\"200804161234\">\n<pre></pre>\n</div>";
 		value_of(actual).should_be(expected);
 	},
 	'Saving tiddler with text': function() {
 		tiddler = new Tiddler("test");
 		tiddler.text = "text";
-		tiddler.created = new Date(Date.UTC(2008,3,16));
+		tiddler.created = version.date;
 		tiddler.modified = tiddler.created;
 
 		var actual = saver.externalizeTiddler(store,tiddler);
-		var expected = "<div title=\"test\" created=\"200804160000\">\n<pre>text</pre>\n</div>";
+		var expected = "<div title=\"test\">\n<pre>text</pre>\n</div>";
 		value_of(actual).should_be(expected);
 	}
 });
