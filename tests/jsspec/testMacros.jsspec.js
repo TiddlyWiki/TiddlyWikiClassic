7e5779b7156841c59a921a49d9a15226b5dd1461
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-16 16:52:04 +0000

    tests: macros; various adjustments

--- tests/jsspec/testMacros.jsspec.js
+++ tests/jsspec/testMacros.jsspec.js
@@ -1,19 +1,16 @@
 // <![CDATA[
-function __main() {
-	store = new TiddlyWiki();
-	loadShadowTiddlers();
-	formatter = new Formatter(config.formatters);
-}
-
 describe('Macros: text macros', {
+	before_each : function() {
+		store = new TiddlyWiki();
+		loadShadowTiddlers();
+		formatter = new Formatter(config.formatters);
+	},
 
-        before_each : function() {
-		__main();
-        },
-        'version macro should expand to the version string' : function() { 
-		var v = version.major + "." + version.minor + "." + version.revision + (version.beta ? " (beta " + version.beta + ")" : "")
-                value_of(wikifyStatic('<<version>>')).should_be("<span>"+v+"</span>");
-        },
+	'version macro should expand to the version string' : function() { 
+		var actual = wikifyStatic("<<version>>");
+		var expected = version.major + "." + version.minor + "." + version.revision
+		 	+ (version.beta ? " (beta " + version.beta + ")" : "");
+		value_of(actual).should_be("<span>" + expected + "</span>");
+	}
 });
-
 // ]]>
