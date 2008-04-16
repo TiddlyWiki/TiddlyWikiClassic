cb96889265978495114129c5e131e32fb5aadb7f
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-16 16:11:05 +0000

    tests: messages; minor clarifications

--- tests/jsspec/testMessages.jsspec.js
+++ tests/jsspec/testMessages.jsspec.js
@@ -1,6 +1,6 @@
 // <![CDATA[
-describe(' test the DisplayMessages', {
-	' should fail if the messageArea is not on the page.': function() {
+describe('displayMessage', {
+	'should fail if the messageArea element does not exist': function() {
 		var actual = displayMessage('test value');
 		var expected = undefined;
 		value_of(actual).should_be(expected);
