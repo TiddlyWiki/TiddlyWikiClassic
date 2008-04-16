5ab0c4ed640560f40343d71693a353ff873b429c
jayfresh <jayfresh@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-16 12:07:22 +0000

    TiddlyWiki core testing: added more tests to testWikifier

--- tests/jsspec/testWikifier.jsspec.js
+++ tests/jsspec/testWikifier.jsspec.js
@@ -1,5 +1,85 @@
 // <![CDATA[
-describe('Wikifier', {
+describe('Wikifier : getParser()', {
+
+	before_each: function(){
+		var formatter = new Formatter(config.formatters);
+	},
+	
+	'it should return the default formatter if no tiddler argument is provided': function() {
+		var actual = getParser(null,null);
+		var expected = formatter;
+		value_of(actual).should_be(expected);
+	},
+	
+	'it should return the default formatter if no format argument is provided and the tiddler has no "wikiformat" field and is not tagged with the value of formatTag of a member of config.parsers': function() {
+		var t = new Tiddler("t");
+		var actual = getParser(t,null);
+		var expected = formatter;
+		value_of(actual).should_be(expected);
+	},
+	
+	'it should return the default formatter if a format argument is provided, but does not appear as a value of formatTag of a member of config.parsers; the tiddler has no "wikiformat" field and is not tagged with the value of formatTag from a member of config.parsers': function() {
+		var t = new Tiddler("t");
+		var actual = getParser(t,"nomatch");
+		var expected = formatter;
+		value_of(actual).should_be(expected);
+	},
+	
+	'it should return the default formatter if the tiddler has a "wikiformat" field that does not appear as a value of formatTag of a member of config.parsers; no format argument is provided and the tiddler is not tagged with the value of formatTag from a member of config.parsers': function() {
+		var t = new Tiddler("t");
+		t.fields.wikiformat = "nomatch";
+		var actual = getParser(t,null);
+		var expected = formatter;
+		value_of(actual).should_be(expected);
+	},
+	
+	'it should return the formatter specified by the "wikiformat" field even if a format tag is provided; no format parameter is provided': function() {
+		var t = new Tiddler("t");
+		t.fields.wikiformat = "format_field";
+		t.tags.push("format_tag");
+		config.parsers.field = {
+			format: "format_field"
+		};
+		config.parsers.tag = {
+			formatTag: "format_tag"
+		};
+		var actual = getParser(t,null);
+		var expected = config.parsers.field;
+		value_of(actual).should_be(expected);
+	},
+	
+	'it should return the formatter specified by the format tag; the tiddler has no "wikiformat" field and no format parameter is provided': function() {
+		var t = new Tiddler("t");
+		t.tags.push("format_tag");
+		config.parsers.tag = {
+			formatTag: "format_tag"
+		};
+		var actual = getParser(t,null);
+		var expected = config.parsers.tag;
+		value_of(actual).should_be(expected);
+	},
+	
+	'it should return the formatter specified by the format parameter even if a format tag and a "wikiformat" field are provided': function() {
+		var t = new Tiddler("t");
+		t.fields.wikiformat = "format_field";
+		t.tags.push("format_tag");
+		config.parsers.field = {
+			format: "format_field"
+		};
+		config.parsers.tag = {
+			formatTag: "format_tag"
+		};
+		config.parsers.parameter = {
+			format: "format_parameter"
+		};
+		var actual = getParser(t,"format_parameter");
+		var expected = config.parsers.parameter;
+		value_of(actual).should_be(expected);
+	}
+});
+
+describe('Wikifier : wikifyStatic()', {
+
 	'Bold formatting': function() {
 		formatter = new Formatter(config.formatters);
 		var actual = wikifyStatic("''bold''");
@@ -31,5 +111,6 @@ describe('Wikifier', {
 		value_of(actual).should_be(expected);
 	}
 });
+
 // ]]>
 
