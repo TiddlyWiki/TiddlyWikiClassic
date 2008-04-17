4c16942f3bb39b3a96458ce43e95f74e4aa623a0
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-17 11:49:13 +0000

    testing - added today macro test

--- tests/jsspec/testMacros.jsspec.js
+++ tests/jsspec/testMacros.jsspec.js
@@ -7,10 +7,16 @@ describe('Macros: text macros', {
 	},
 
 	'version macro should expand to the version string' : function() { 
-		var actual = wikifyStatic("<<version>>");
-		var expected = version.major + "." + version.minor + "." + version.revision
-		 	+ (version.beta ? " (beta " + version.beta + ")" : "");
-		value_of(actual).should_be("<span>" + expected + "</span>");
-	}
+		version.major = "123";
+		version.minor = "456";
+		version.revision = "789";
+		version.beta = "123456789";
+		value_of(wikifyStatic("<<version>>")).should_be("<span>123.456.789 (beta 123456789)</span>");
+	},
+
+	'today macro should return a date shaped string' : function() { 
+		value_of(wikifyStatic("<<today>>")).should_match(/^<span>[A-Z][a-z]+\s[A-Z][a-z]+\s[0-9]{2}\s([0-9]){2}:[0-9]{2}:[0-9]{2} 2[0-9]{3}<\/span>$/);
+	},
+
 });
 // ]]>
