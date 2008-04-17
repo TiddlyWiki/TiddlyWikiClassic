2d53e44e5e8b72853e9ffa6b6468db24a2893652
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-17 16:19:59 +0000

    Fixed to take into account fact that HTML tags can be uppercase in some browsers

--- tests/jsspec/testWikifier.jsspec.js
+++ tests/jsspec/testWikifier.jsspec.js
@@ -82,32 +82,32 @@ describe('Wikifier : wikifyStatic()', {
 
 	'Bold formatting': function() {
 		formatter = new Formatter(config.formatters);
-		var actual = wikifyStatic("''bold''");
-		var expected = "<strong>bold</strong>";		
+		var actual = (wikifyStatic("''bold''")).toLowerCase();
+		var expected = "<strong>bold</strong>";
 		value_of(actual).should_be(expected);
 	},
 	'Italic formatting': function() {
 		formatter = new Formatter(config.formatters);
-		var actual = wikifyStatic("//italic//");
-		var expected = "<em>italic</em>";		
+		var actual = wikifyStatic("//italic//").toLowerCase();;
+		var expected = "<em>italic</em>";
 		value_of(actual).should_be(expected);
 	},
 	'Underline formatting': function() {
 		formatter = new Formatter(config.formatters);
-		var actual = wikifyStatic("__underline__");
-		var expected = "<u>underline</u>";		
+		var actual = wikifyStatic("__underline__").toLowerCase();;
+		var expected = "<u>underline</u>";
 		value_of(actual).should_be(expected);
 	},
 	'Superscript formatting': function() {
 		formatter = new Formatter(config.formatters);
-		var actual = wikifyStatic("^^superscript^^");
-		var expected = "<sup>superscript</sup>";		
+		var actual = wikifyStatic("^^superscript^^").toLowerCase();;
+		var expected = "<sup>superscript</sup>";
 		value_of(actual).should_be(expected);
 	},
 	'Subscript formatting': function() {
 		formatter = new Formatter(config.formatters);
-		var actual = wikifyStatic("~~subscript~~");
-		var expected = "<sub>subscript</sub>";		
+		var actual = wikifyStatic("~~subscript~~").toLowerCase();;
+		var expected = "<sub>subscript</sub>";
 		value_of(actual).should_be(expected);
 	}
 });
