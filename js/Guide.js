3ccdd3530913e49ab9fd6ad0718af0c5ca56ad8f
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-17 11:32:21 +0000

    Removed spurious whitespace characters

--- js/Guide.js
+++ js/Guide.js
@@ -1,10 +1,10 @@
 //
 // Please note:
-// 
+//
 // * This code is designed to be readable but for compactness it only includes brief comments. You can see fuller comments
 //   in the project Subversion repository at http://svn.tiddlywiki.org/Trunk/core/
 //
 // * You should never need to modify this source code directly. TiddlyWiki is carefully designed to allow deep customisation
 //   without changing the core code. Please consult the development group at http://groups.google.com/group/TiddlyWikiDev
-// 
+//
 
