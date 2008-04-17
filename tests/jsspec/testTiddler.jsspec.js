29799b279b281da6dd02c0a336d9a50a0b80a74c
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-17 10:45:12 +0000

    More tests

--- tests/jsspec/testTiddler.jsspec.js
+++ tests/jsspec/testTiddler.jsspec.js
@@ -1,5 +1,13 @@
 // <![CDATA[
 describe('Tiddlers', {
+	'Newly created tiddler should have empty string as content': function() {
+		var tiddler = new Tiddler("temp");
+		value_of(tiddler.text).should_be("");
+	},
+	'Created and modified dates should be equal for newly created tiddler': function() {
+		var tiddler = new Tiddler("temp");
+		value_of(tiddler.created).should_be(tiddler.modified);
+	},
 	'Tiddler changecount': function() {
 		var tiddler = new Tiddler("temp");
 		value_of(tiddler.isTouched()).should_be(false);
@@ -8,9 +16,8 @@ describe('Tiddlers', {
 		tiddler.clearChangeCount();
 		value_of(tiddler.isTouched()).should_be(false);
 	},
-	'Tiddler tags': function() {
+	'Newly created tiddler should not have any tags': function() {
 		var tiddler = new Tiddler("temp");
-		value_of(tiddler.isTagged()).should_be(false);
 		value_of(tiddler.getTags()).should_be("");
 	}
 });
