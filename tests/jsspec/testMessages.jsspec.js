5d928268d35f23db86a5336fd3cab4399ca9a080
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-21 13:16:31 +0000

    tests: enhanced displayMessage tests

--- tests/jsspec/testMessages.jsspec.js
+++ tests/jsspec/testMessages.jsspec.js
@@ -1,25 +1,14 @@
 // <![CDATA[
-describe('displayMessage', {
-	before_each: function() {
-		createTiddlyElement(document.body,"div","messageArea");
-	},
+function getMessage() {
+	return msgArea.getElementsByTagName("div")[1].innerHTML;
+}
 
-	'should fail if the messageArea element does not exist': function() {
-		var actual = displayMessage('test value');
-		var expected = undefined;
-		value_of(actual).should_be(expected);
-	},
-	
-	'should provide the letter "s" contained in a message area.': function() {
-		createTiddlyElement(document.body,"div","messageArea");
-	 	displayMessage('s');
-		var a = document.getElementById('messageArea');
-		actual = a.getElementsByTagName('div')[1].innerHTML;
-		var expected = 's';
-		value_of(actual).should_be(expected);
-	}
+describe('displayMessage', {	before_each: function() {		msgArea = createTiddlyElement(document.body,"div","messageArea");
+		msgArea.style.visibility = "hidden";	},	after_each: function() {		removeNode(msgArea);	},
 
-});
-// ]]>
-
-
+	/* NOTE: disabled to suppress alert()	'should fail if the messageArea element does not exist': function() {
+		msgArea.id = "messageArea_disabled";
+		displayMessage("test value");
+		var actual = getMessage();		var expected = undefined;		value_of(actual).should_be(expected);	},
+	*/
+	'should provide the letter "s" contained in a message area': function() {		displayMessage("s");		var actual = getMessage();		var expected = "s";		value_of(actual).should_be(expected);	}});// ]]>
