2a56d055180398b021a91f35b8a4d5d8515f04ee
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-21 10:30:39 +0000

    displayMessage test

--- tests/jsspec/testMessages.jsspec.js
+++ tests/jsspec/testMessages.jsspec.js
@@ -8,6 +8,18 @@ describe('displayMessage', {
 		var actual = displayMessage('test value');
 		var expected = undefined;
 		value_of(actual).should_be(expected);
+	},
+	
+	'should provide the letter "s" contained in a message area.': function() {
+		createTiddlyElement(document.body,"div","messageArea");
+	 	displayMessage('s');
+		var a = document.getElementById('messageArea');
+		actual = a.getElementsByTagName('div')[1].innerHTML;
+		var expected = 's';
+		value_of(actual).should_be(expected);
 	}
+
 });
 // ]]>
+
+
