be0e522c9ef04640c96f3fbeea646ac0ec787227
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-23 08:38:11 +0000

    testing: slices

---
+++ tests/jsspec/testTiddlyWiki.jsspec.js
@@ -0,0 +1,61 @@
+// <![CDATA[
+describe('Slices', {
+	before_each: function() {
+		store = new TiddlyWiki();
+		//loadShadowTiddlers();
+		//store.saveTiddler("t","t","text");
+		//formatter = new Formatter(config.formatters);
+	},
+
+	'calcAllSlices() should return an object when not passed any arguments': function() {
+		var actual = typeof store.calcAllSlices();
+		var expected = "object";
+		value_of(actual).should_be(expected);
+	},
+	'calcAllSlices() should return an object when passed an empty string': function() {
+		var actual = typeof store.calcAllSlices("");
+		var expected = "object";
+		value_of(actual).should_be(expected);
+	},
+	'calcAllSlices() should return an object when pointed to a non-existing tiddler': function() {
+		var actual = typeof store.calcAllSlices("MissingTiddler");
+		var expected = "object";
+		value_of(actual).should_be(expected);
+	},
+
+	'calcAllSlices() should return an existing slice (colon notation) as a label/value pair': function() {
+		var title = "tiddler";
+		var text = "foo: bar";
+		store.saveTiddler(title, title, text);
+		var actual = store.calcAllSlices(title);
+		var expected = { foo: "bar" };
+		value_of(actual).should_be(expected);
+	},
+	'calcAllSlices() should return existing slices (colon notation) as label/value pairs': function() {
+		var title = "tiddler";
+		var text = "foo: bar\n"
+			+ "lorem: ipsum";
+		store.saveTiddler(title, title, text);
+		var actual = store.calcAllSlices(title);
+		var expected = { foo: "bar", lorem: "ipsum" };
+		value_of(actual).should_be(expected);
+	},
+	'calcAllSlices() should return an existing slice (table notation) as a label/value pair': function() {
+		var title = "tiddler";
+		var text = "|foo|bar|";
+		store.saveTiddler(title, title, text);
+		var actual = store.calcAllSlices(title);
+		var expected = { foo: "bar" };
+		value_of(actual).should_be(expected);
+	},
+	'calcAllSlices() should return existing slices (table notation) as label/value pairs': function() {
+		var title = "tiddler";
+		var text = "|foo|bar|\n"
+			+ "|lorem|ipsum|";
+		store.saveTiddler(title, title, text);
+		var actual = store.calcAllSlices(title);
+		var expected = { foo: "bar", lorem: "ipsum" };
+		value_of(actual).should_be(expected);
+	}
+});
+// ]]>
