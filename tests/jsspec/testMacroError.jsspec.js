38bc3d0aa84e4e1013e34cd29b38cc081e51c612
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-24 11:17:38 +0000

    test for macro that does not exist

---
+++ tests/jsspec/testMacroError.jsspec.js
@@ -0,0 +1,26 @@
+// <![CDATA[
+
+function __main() {
+	store = new TiddlyWiki();
+	loadShadowTiddlers();
+	formatter = new Formatter(config.formatters);
+
+	
+}
+
+describe('Macros: list macro', {
+	before_each : function() {
+		__main();
+	},
+
+	'list shadowed by default expands to the listTitle and a list of builtin shadowed tiddlers' : function() { 
+		
+	var input = wikifyStatic('<<NOEXISTANTMACRO>>');
+	var should_be = '<a errortext="Error while executing macro &lt;&lt;NOEXISTANTMACRO&gt;&gt;:\nNo such macro" class="errorButton" href="javascript:;">Error in macro &lt;&lt;NOEXISTANTMACRO&gt;&gt;</a>';
+
+value_of(input).should_be(should_be);
+	},
+
+});
+
+// ]]>
