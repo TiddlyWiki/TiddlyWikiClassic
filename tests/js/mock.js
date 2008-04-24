a47f36c3a09b5bc5de61776f2d01b14d8c79747b
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-24 13:53:57 +0000

    testing - mock a named function

--- tests/js/mock.js
+++ tests/js/mock.js
@@ -1,44 +1,22 @@
-// lovingly nicked and adapted from message macro
-function resolveVar(varString) {
-	if(!varString)
-		return null;	
-	var names = varString.split(".");
-	var lookupMessage = function(root,nameIndex) {
-		if(names[nameIndex] in root) {
-			if(nameIndex < names.length-1)
-				return (lookupMessage(root[names[nameIndex]],nameIndex+1));
-			else
-				return root[names[nameIndex]];
-		} else
-			return null;
-	};
-	return lookupMessage(window,0);
-}
+tests_mock = {
+	saved:  {},
+	before: function(funcName)
+	{
+		var frame = {};
+		frame.savedFunc = eval(funcName); 
+		frame.was_called = 0;
 
-function mock_once(funcToMock,mocker) {
-	var parent, child;
-	
-	var mockVars = {
-		called:false // this could have as many default properties as we wanted to track behaviour
-	};
-	mocker.mockVars = mockVars; // attach mockVars to the mocker so we can get at it from the mocker using arguments.callee.mockVars
-	
-	// translate the funcToMock string to an object reference and store the current function
-	if(funcToMock.indexOf(".") == -1) {
-		parent = window;
-		child = funcToMock;
-	} else {
-		parent = resolveVar(funcToMock.substring(0,funcToMock.lastIndexOf(".")));
-		child = funcToMock.substring(funcToMock.lastIndexOf(".")+1);
+		var mockFunction = function() { 
+			tests_mock.saved[funcName].was_called = 1; 
+		}
+		eval(funcName + "=mockFunction");
+
+		this.saved[funcName] = frame;
+	},
+	after: function(funcName) 
+	{
+		frame = this.saved[funcName];
+		eval(funcName + '=frame.savedFunc');
+		return frame.was_called;
 	}
-	var original = parent[child];
-	
-	// override the current function
-	parent[child] = function() {
-		mocker.apply(this,arguments);
-		mockVars.called = true;
-		parent[child] = original;
-	};
-	
-	return mockVars;
-}
+};
