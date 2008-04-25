79cad3579505483255d4796b65bd16668a11c570
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-25 09:54:29 +0000

    Rename

---
+++ tests/jsspec/Crypto.jsspec.js
@@ -0,0 +1,28 @@
+// <![CDATA[
+describe('Crypto: hexSha1Str()', {
+
+	'SHA-1 hash of empty string should be correct': function() {
+		var actual = Crypto.hexSha1Str("").toLowerCase();
+		var expected = "da39a3ee5e6b4b0d3255bfef95601890afd80709";
+		value_of(actual).should_be(expected);
+	},
+	'SHA-1 hash of test vector 1 should be correct': function() {
+		var actual = Crypto.hexSha1Str("The quick brown fox jumps over the lazy dog").toLowerCase();
+		var expected = "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12";
+		value_of(actual).should_be(expected);
+	},
+	'SHA-1 hash of test vector 2 should be correct': function() {
+		var actual = Crypto.hexSha1Str("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq");
+		var expected = "84983E441C3BD26EBAAE4AA1F95129E5E54670F1";
+		value_of(actual).should_be(expected);
+	},
+	'SHA-1 hash of test vector 3 should be correct': function() {
+		var actual = Crypto.hexSha1Str("12345678901234567890123456789012345678901234567890123456789012345678901234567890");
+		var expected = "50ABF5706A150990A08B2C5EA40FA0E585554732";
+		value_of(actual).should_be(expected);
+	}
+	
+
+});
+// ]]>
+
