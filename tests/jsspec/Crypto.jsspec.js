0ddfea5a556498f8ce82f6f166010aa9a2c7f433
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-28 12:47:43 +0000

    More tests

--- tests/jsspec/Crypto.jsspec.js
+++ tests/jsspec/Crypto.jsspec.js
@@ -12,11 +12,16 @@ describe('Crypto: hexSha1Str()', {
 		value_of(actual).should_be(expected);
 	},
 	'SHA-1 hash of test vector 2 should be correct': function() {
+		var actual = Crypto.hexSha1Str("abc").toLowerCase();
+		var expected = "a9993e364706816aba3e25717850c26c9cd0d89d";
+		value_of(actual).should_be(expected);
+	},
+	'SHA-1 hash of test vector 3 should be correct': function() {
 		var actual = Crypto.hexSha1Str("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq");
 		var expected = "84983E441C3BD26EBAAE4AA1F95129E5E54670F1";
 		value_of(actual).should_be(expected);
 	},
-	'SHA-1 hash of test vector 3 should be correct': function() {
+	'SHA-1 hash of test vector 4 should be correct': function() {
 		var actual = Crypto.hexSha1Str("12345678901234567890123456789012345678901234567890123456789012345678901234567890");
 		var expected = "50ABF5706A150990A08B2C5EA40FA0E585554732";
 		value_of(actual).should_be(expected);
