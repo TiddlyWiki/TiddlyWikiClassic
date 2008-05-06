950e9f68f2249d02af684f1cf8b14c5ae71d8fba
jayfresh <jayfresh@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-05-06 11:37:04 +0000

    TiddlyWiki core tests: added tests for compareVersions

--- tests/jsspec/Utilities.jsspec.js
+++ tests/jsspec/Utilities.jsspec.js
@@ -10,7 +10,7 @@ describe('Utilities: formatVersion', {
 			minor:2,
 			revision:3,
 			beta:true
-		}
+		};
 	},
 	'it should use a version object if one is passed as a parameter, which has properties: major, minor, revision, beta (optional) and format the output as "major.minor.revision"': function() {
 		var actual = formatVersion(v);
@@ -36,3 +36,94 @@ describe('Utilities: formatVersion', {
 		delete v_beta;
 	}
 });
+
+describe('Utilities: compareVersions', {
+	before_each: function() {
+		v1 = {
+			major:1,
+			minor:2,
+			revision:3
+		};
+		v1_beta = {
+			major:1,
+			minor:2,
+			revision:3,
+			beta:true
+		};
+		v2 = {
+			major:v1.major,
+			minor:v1.minor,
+			revision:v1.revision
+		};
+	},
+
+	'it should return +1 if the second version is later than the first': function() {
+		v2.major = v1.major+1;
+		var actual = compareVersions(v1,v2);
+		var expected = 1;
+		value_of(actual).should_be(expected);
+		v2.major--;
+		v2.minor = v1.minor+1;
+		actual = compareVersions(v1,v2);
+		expected = 1;
+		value_of(actual).should_be(expected);
+		v2.minor--;
+		v2.revision = v1.revision+1;
+		actual = compareVersions(v1,v2);
+		expected = 1;
+		value_of(actual).should_be(expected);
+	},
+	
+	'it should return 0 if the second version is the same as the first': function() {
+		var actual = compareVersions(v1,v2);
+		var expected = 0;
+		value_of(actual).should_be(expected);
+	},
+	
+	'it should return -1 if the second version is earlier than the first': function() {
+		v2.major = v1.major-1;
+		var actual = compareVersions(v1,v2);
+		var expected = -1;
+		value_of(actual).should_be(expected);
+		v2.major++;
+		v2.minor = v1.minor-1;
+		actual = compareVersions(v1,v2);
+		expected = -1;
+		value_of(actual).should_be(expected);
+		v2.minor++;
+		v2.revision = v1.revision-1;
+		actual = compareVersions(v1,v2);
+		expected = -1;
+		value_of(actual).should_be(expected);
+	},
+	
+	'it should treat versions without a beta number as later than a version without a beta number': function() {
+		var actual = compareVersions(v1,v1_beta);
+		var expected = -1;
+		value_of(actual).should_be(expected);
+	}
+
+});
+
+//# Compares two TiddlyWiki version objects
+//# Returns +1 if v2 is later than v1
+//#          0 if v2 is the same as v1
+//#         -1 if v2 is earlier than v1
+//# version without a beta number is later than a version with a beta number
+compareVersions = function(v1,v2)
+{
+	var a = ["major","minor","revision"];
+	for(var i = 0; i<a.length; i++) {
+		var x1 = v1[a[i]] || 0;
+		var x2 = v2[a[i]] || 0;
+		if(x1<x2)
+			return 1;
+		if(x1>x2)
+			return -1;
+	}
+	x1 = v1.beta || 9999;
+	x2 = v2.beta || 9999;
+	if(x1<x2)
+		return 1;
+	return x1 > x2 ? -1 : 0;
+};
