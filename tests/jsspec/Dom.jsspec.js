772502e98e1ff4364ca72c6d24d88001bde4a371
PhilHawksworth <PhilHawksworth@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-05-14 11:35:27 +0000

    Testing: Added tests for getElementsByClassName to the Dom.js tests

--- tests/jsspec/Dom.jsspec.js
+++ tests/jsspec/Dom.jsspec.js
@@ -1,5 +1,21 @@
 // <![CDATA[
-describe('TiddlyWiki DOM element creation', {
+
+function __createDomElementsForQuery() {
+	var place = document.body;
+	var d = document.createElement("div");
+	d.id = "test_div";
+	place.appendChild(d);
+	for (var i=0; i < 4; i++) {
+		p = document.createElement("p");
+		p.className = "test_p";
+		d.appendChild(p);
+	};
+	p = document.createElement("p");
+	p.className = "test_p";
+	place.appendChild(p);
+}
+
+describe('Dom : TiddlyWiki DOM element creation', {
 	'Element creation should create the DOM element': function() {
 		var actual = createTiddlyElement(null,"div");
 		value_of(actual).should_not_be_null();
@@ -22,4 +38,27 @@ describe('TiddlyWiki DOM element creation', {
 		value_of(actual).should_be(expected);
 	}
 });
+
+describe('Dom : getElementsByClassName', {
+
+	'document should have a getElementsByClassName function' : function() {
+		var g = document.getElementsByClassName;
+		value_of(typeof g).should_be('function');
+	},
+	
+	'document.getElementsByClassName() returns an array of elements ' : function() {
+		__createDomElementsForQuery();
+		var results = document.getElementsByClassName('test_p');
+		value_of(results).should_have_exactly(5, "items");
+	},
+	
+	'node.getElementsByClassName() returns an array of elements with the specified class who are children of the node' : function() {
+		__createDomElementsForQuery();
+		var n = document.getElementById('test_div');
+		var results = n.getElementsByClassName('test_p');
+		value_of(results).should_have_exactly(4, "items");
+	}
+
+});
+
 // ]]>
