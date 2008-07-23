e5e74cfce0cb07bba3743d942aaf7c947e8752df
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-07-23 15:50:04 +0000

    Updated version number

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 4, revision: 1, beta: 1, date: new Date("Jun 11, 2008"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 4, revision: 1, beta: 2, date: new Date("Jul 23, 2008"), extensions: {}};
