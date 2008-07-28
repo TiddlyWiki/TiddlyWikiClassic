609ee35a11066e0a76c9c7f45ae4b5d10e935ded
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-07-28 13:08:44 +0000

    Preparing for beta 3

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 4, revision: 1, beta: 2, date: new Date("Jul 23, 2008"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 4, revision: 1, beta: 3, date: new Date("Jul 23, 2008"), extensions: {}};
