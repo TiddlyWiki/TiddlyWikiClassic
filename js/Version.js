ce264dd63bccd4faf3f48605cf953f65949e56ca
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-28 16:32:57 +0000

    increased TiddlyWiki version number to 2.5.0 beta 1

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 4, revision: 1, date: new Date("Aug 4, 2008"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 5, revision: 0, beta: 1, date: new Date("Oct 28, 2008"), extensions: {}};
