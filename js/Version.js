3a60fcd983136f64f64a87d6217c051b2cab038d
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-11-17 16:43:20 +0000

    version info: changed "beta" to "alpha"

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 5, revision: 0, beta: 1, date: new Date("Oct 28, 2008"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 5, revision: 0, alpha: 1, date: new Date("Oct 28, 2008"), extensions: {}};
