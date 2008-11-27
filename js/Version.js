abb2742e4bea1c978c09221230913ee403a7808d
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-11-27 14:43:43 +0000

    Set version to 2.4.2

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 5, revision: 0, alpha: 1, date: new Date("Oct 28, 2008"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 4, revision: 2, alpha: 1, date: new Date("Nov 27, 2008"), extensions: {}};
