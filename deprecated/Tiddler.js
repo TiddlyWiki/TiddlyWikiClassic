7dee4f9435f07cf9f703e3e3a154e0d5c86aa8a5
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-12-09 17:19:55 +0000

    Minor fix

--- deprecated/Tiddler.js
+++ deprecated/Tiddler.js
@@ -2,13 +2,13 @@
 //-- Deprecated Tiddler code
 //--
 
-/// @Deprecated: Use tiddlerToRssItem(tiddler,uri) instead
+// @Deprecated: Use tiddlerToRssItem(tiddler,uri) instead
 Tiddler.prototype.toRssItem = function(uri)
 {
 	return tiddlerToRssItem(this,uri);
 };
 
-// / @Deprecated: Use "<item>\n" + tiddlerToRssItem(tiddler,uri)  + "\n</item>" instead
+// @Deprecated: Use "<item>\n" + tiddlerToRssItem(tiddler,uri)  + "\n</item>" instead
 Tiddler.prototype.saveToRss = function(uri)
 {
 	return "<item>\n" + tiddlerToRssItem(this,uri) + "\n</item>";
