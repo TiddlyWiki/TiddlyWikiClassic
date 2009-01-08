c53a001e56e875a87ca7ef2db74d3f05a0c59069
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-01-08 16:09:28 +0000

    Ticket #757
    add removeOptionCookie() function to core

--- js/Options.js
+++ js/Options.js
@@ -42,6 +42,11 @@ function saveOptionCookie(name)
 	document.cookie = c;
 }
 
+function removeCookie(name)
+{
+	document.cookie = name + "=; expires=Thu, 01-Jan-1970 00:00:01 UTC; path=/;";
+}
+
 function encodeCookie(s)
 {
 	return escape(convertUnicodeToHtmlEntities(s));
