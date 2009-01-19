b1dd18b7c6df013b9bd6b7d2554aeda2f7e8cf6f
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-01-19 17:06:30 +0000

    added explanatory comment

--- jquery/plugins/jquery.tw.js
+++ jquery/plugins/jquery.tw.js
@@ -4,6 +4,7 @@ addition of tw 'namespace'
 */
 (function($) {
 	if(!$.tw) {
+		//# make TiddlyWiki namespace act like jQuery namespace (jQuery.tw.fn imitates jQuery.fn)
 		$.tw = function(selector,context){return new $.tw.fn.init(selector,context);};
 		$.tw.fn = $.tw.prototype = function(){return $.tw.fn;};
 		$.tw.fn.init = function(selector,context){return $.tw.fn;};
