8342789edcfa58b3832b77a7380752807219f598
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-03-05 14:07:26 +0000

    testing: Basic Types; Number clamp

--- qunit/tests/testBasicTypes.js
+++ qunit/tests/testBasicTypes.js
@@ -1,5 +1,30 @@
-jQuery(document).ready(function(){
+jQuery(document).ready(function() {
 	module("Basic Types");
+
+	test("Number clamp", function() {
+		expect(5);
+		var actual, expected;
+
+		actual = (99).clamp();
+		expected = 99;
+		equals(actual, expected, "return original number if no range is given");
+
+		actual = (11).clamp(20);
+		expected = 20;
+		equals(actual, expected, "if only one argument is given, use it as minimum");
+
+		actual = (55).clamp(20, 80);
+		expected = 55;
+		equals(actual, expected, "return original number if it is between minimum and maximum");
+
+		actual = (11).clamp(20, 80);
+		expected = 20;
+		equals(actual, expected, "return minimum if number is smaller than minimum");
+
+		actual = (99).clamp(20, 80);
+		expected = 80;
+		equals(actual, expected, "return maximum if number is greater than maximum");
+	});
 
 	test("Array indexOf", function() {
 		expect(3);
