a79ed06b91af95bfa5843f35b29d3e481119703a
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-03-05 15:35:12 +0000

    testing: Basic Types; Array methods

--- qunit/tests/testBasicTypes.js
+++ qunit/tests/testBasicTypes.js
@@ -7,11 +7,11 @@ jQuery(document).ready(function() {
 
 		actual = (99).clamp();
 		expected = 99;
-		equals(actual, expected, "return original number if no range is given");
+		equals(actual, expected, "return original number if no range is specified");
 
 		actual = (11).clamp(20);
 		expected = 20;
-		equals(actual, expected, "if only one argument is given, use it as minimum");
+		equals(actual, expected, "if only one argument is specified, use it as minimum");
 
 		actual = (55).clamp(20, 80);
 		expected = 55;
@@ -41,5 +41,44 @@ jQuery(document).ready(function() {
 		actual = ["foo", "bar"].indexOf("baz");
 		expected = -1;
 		equals(actual, expected, "returns -1 if element not found");
+	});
+
+	test("Array findByField", function() {
+		expect(3);
+		var actual, expected;
+
+		var L = [{ foo: "lorem", bar: "ipsum" }, { bar: "dolor", baz: "sit" }, { bar: "dolor" }];
+
+		actual = L.findByField();
+		expected = 0;
+		equals(actual, expected, "returns 0 if no arguments are specified"); // XXX: not actually desired; cf. ticket #964
+
+		actual = L.findByField("bar", "dolor");
+		expected = 1;
+		equals(actual, expected, "returns the position of the first matching element");
+
+		actual = L.findByField("bar", "xxx");
+		expected = null;
+		equals(actual, expected, "returns null if no match was found"); // XXX: not actually desired; cf. ticket #966
+
+	});
+
+	test("Array contains", function() {
+		expect(3);
+		var actual, expected;
+
+		var L = ["foo", "bar", "baz", "bar"];
+
+		actual = L.contains();
+		expected = false;
+		equals(actual, expected, "returns false if no arguments are specified"); // XXX: not actually desired; cf. ticket #966
+
+		actual = L.contains("bar");
+		expected = 1;
+		equals(actual, expected, "returns the position of the first matching element");
+
+		actual = L.contains("xxx");
+		expected = false;
+		equals(actual, expected, "returns false if no match was found"); // XXX: not actually desired; cf. ticket #966
 	});
 });
