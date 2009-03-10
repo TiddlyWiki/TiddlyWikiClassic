4edf2b7ddd8333d449b787dd6c42091a40eb102e
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-03-10 12:31:58 +0000

    testing: DOM; proper teardown

--- qunit/tests/testDOM.js
+++ qunit/tests/testDOM.js
@@ -27,14 +27,14 @@ jQuery(document).ready(function(){
 		ok(!hasClass(ele, 'testClass'), "hasClass() returns false when looking for a class which is not present on an element");
 		jq(ele).addClass('testClass');
 		ok(hasClass(ele, 'testClass'), "hasClass() returns true when looking for a class which is present on an element");
-		
+
 	});
 
 
 	test("Events", function() {
 		expect(1);
 		var ele = makeTestNode();
-		
+
 		// resolveTarget()
 		var target;
 		jq(ele).click(function(ev){
@@ -43,12 +43,13 @@ jQuery(document).ready(function(){
 		jq(ele).click();
 		equals(target, ele, "resolveTarget correctly identifies the target of a click event");
 	});
-	
-	
+
+
 	test('Text', function(){
 		jq('body').append("<div id='text_test'>foo bar baz</div>");
 		var d = document.getElementById('text_test');
 		equals(getPlainText(d), "foo bar baz", "getPlainText() returns the plain text of an element.");
+		jq("#text_test").remove();
 	});
-	
+
 });
