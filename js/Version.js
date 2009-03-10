755367bb496f77534e6121992bf27910f25fd9e0
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-03-10 11:59:19 +0000

    TiddlyWiki: post release alpha version increment

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 5, revision: 0, date: new Date("Mar 9, 2009"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 5, revision: 1, alpha: 1, date: new Date("Mar 9, 2009"), extensions: {}};
