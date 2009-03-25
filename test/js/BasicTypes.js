5c9d9aed92cc16d6769d57050db90c8e94f241ba
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-03-25 16:47:50 +0000

    TiddlyTest - uncommented clamp test code following fix to clamp function

--- test/js/BasicTypes.js
+++ test/js/BasicTypes.js
@@ -4,21 +4,17 @@ jQuery(document).ready(function() {
 	test("Number clamp", function() {
 		var actual, expected;
 
-		/* XXX: fails; cf. #972
 		actual = (99).clamp();
 		expected = 99;
 		same(actual, expected, "returns original number if no arguments are specified");
-		*/
 
 		actual = (11).clamp(20);
 		expected = 20;
 		same(actual, expected, "if only one argument is specified, uses it as minimum");
 
-		/* XXX: fails; cf. #972
 		actual = (55).clamp(20, 80);
 		expected = 55;
 		same(actual, expected, "returns original number if it is between minimum and maximum");
-		*/
 
 		actual = (11).clamp(20, 80);
 		expected = 20;
