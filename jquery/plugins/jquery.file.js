cfd893870eabb35db59be3737556d0a376f8f4e6
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-04-15 15:17:55 +0000

    Ticket #1036 (new defect) jQuery file plugins should be refactored for better standalone usage

--- jquery/plugins/jquery.file.js
+++ jquery/plugins/jquery.file.js
@@ -1,7 +1,7 @@
 /*
-jquery.file.save.js
+jquery.file.js
 
-jQuery plugin for saving data to a file
+jQuery plugin for loading a file and saving data to a file
 
 Copyright (c) UnaMesa Association 2009
 
@@ -12,11 +12,24 @@ Dual licensed under the MIT and GPL licenses:
 
 (function($) {
 
+	$(function() {
+		if(!document.applets["TiddlySaver"] && !$.browser.mozilla && !$.browser.msie) {
+			$(document.body).append("<applet style='position:absolute;left:-1px' name='TiddlySaver' code='TiddlySaver.class' archive='TiddlySaver.jar' width='1'height='1'></applet>");
+		}
+	});
 	if(!$.file) {
 		$.file = {};
 	}
 
 	$.extend($.file,{
+		load: function(filePath) {
+			var r = mozillaLoadFile(filePath);
+			if((r == null) || (r == false))
+				r = ieLoadFile(filePath);
+			if((r == null) || (r == false))
+				r = javaLoadFile(filePath);
+			return r;
+		},
 		save: function(filePath,content) {
 			var r = mozillaSaveFile(filePath,content);
 			if(!r)
@@ -29,6 +42,73 @@ Dual licensed under the MIT and GPL licenses:
 			return $.browser.msie ? ieCopyFile(dest,source) : false;
 		}
 	});
+
+	// Private functions.
+	function ieLoadFile(filePath) {
+		// Returns null if it can't do it, false if there's an error, or a string of the content if successful
+		try {
+			var fso = new ActiveXObject("Scripting.FileSystemObject");
+			var file = fso.OpenTextFile(filePath,1);
+			var content = file.ReadAll();
+			file.Close();
+		} catch(ex) {
+			//# alert("Exception while attempting to load\n\n" + ex.toString());
+			return null;
+		}
+		return content;
+	}
+
+	function mozillaLoadFile(filePath) {
+		// Returns null if it can't do it, false if there's an error, or a string of the content if successful
+		if(window.Components) {
+			try {
+				netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+				var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
+				file.initWithPath(filePath);
+				if(!file.exists())
+					return null;
+				var inputStream = Components.classes["@mozilla.org/network/file-input-stream;1"].createInstance(Components.interfaces.nsIFileInputStream);
+				inputStream.init(file,0x01,4,null);
+				var sInputStream = Components.classes["@mozilla.org/scriptableinputstream;1"].createInstance(Components.interfaces.nsIScriptableInputStream);
+				sInputStream.init(inputStream);
+				var contents = sInputStream.read(sInputStream.available());
+				sInputStream.close();
+				inputStream.close();
+				return contents;
+			} catch(ex) {
+				//# alert("Exception while attempting to load\n\n" + ex);
+				return false;
+			}
+		}
+		return null;
+	}
+
+	function javaUrlToFilename(url) {
+		var f = "//localhost";
+		if(url.indexOf(f) == 0)
+			return url.substring(f.length);
+		var i = url.indexOf(":");
+		return i > 0 ? url.substring(i-1) : url;
+	}
+
+	function javaLoadFile(filePath) {
+		try {
+			if(document.applets["TiddlySaver"])
+				return String(document.applets["TiddlySaver"].loadFile(javaUrlToFilename(filePath),"UTF-8"));
+		} catch(ex) {
+		}
+		var content = [];
+		try {
+			var r = new java.io.BufferedReader(new java.io.FileReader(javaUrlToFilename(filePath)));
+			var line;
+			while((line = r.readLine()) != null)
+				content.push(new String(line));
+			r.close();
+		} catch(ex) {
+			return null;
+		}
+		return content.join("\n");
+	}
 
 	function ieCreatePath(path) {
 		try {
