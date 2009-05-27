8d1a2b484537e280e3f536c4166912a577cd444b
JeremyRuston <JeremyRuston@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-05-27 08:34:27 +0000

    [fileplugin] fixed problem with unnecessarily loading TiddlySaver applet when loaded over HTTP

--- jquery/plugins/jquery.twFile.js
+++ jquery/plugins/jquery.twFile.js
@@ -199,7 +199,7 @@ Triple licensed under the BSD, MIT and GPL licenses:
 	drivers.tiddlySaver = {
 		name: "tiddlySaver",
 		deferredInit: function() {
-			if(!document.applets["TiddlySaver"] && !$.browser.mozilla && !$.browser.msie) {
+			if(!document.applets["TiddlySaver"] && !$.browser.mozilla && !$.browser.msie && document.location.toString().substr(0,5) == "file:") {
 				$(document.body).append("<applet style='position:absolute;left:-1px' name='TiddlySaver' code='TiddlySaver.class' archive='TiddlySaver.jar' width='1'height='1'></applet>");
 			}
 		},
