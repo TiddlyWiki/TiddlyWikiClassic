021ad9d9c89c0341214b0a9da456a18ff773ff7a
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-07-30 10:38:53 +0000

    Ticket #1062
    deprecate toJSONString method

--- js/Strings.js
+++ js/Strings.js
@@ -75,30 +75,6 @@ String.prototype.htmlDecode = function()
 	return this.replace(/&lt;/mg,"<").replace(/&gt;/mg,">").replace(/&quot;/mg,"\"").replace(/&amp;/mg,"&");
 };
 
-// Convert a string to it's JSON representation by encoding control characters, double quotes and backslash. See json.org
-String.prototype.toJSONString = function()
-{
-	var m = {
-		'\b': '\\b',
-		'\f': '\\f',
-		'\n': '\\n',
-		'\r': '\\r',
-		'\t': '\\t',
-		'"' : '\\"',
-		'\\': '\\\\'
-		};
-	var replaceFn = function(a,b) {
-		var c = m[b];
-		if(c)
-			return c;
-		c = b.charCodeAt();
-		return '\\u00' + Math.floor(c / 16).toString(16) + (c % 16).toString(16);
-		};
-	if(/["\\\x00-\x1f]/.test(this))
-		return '"' + this.replace(/([\x00-\x1f\\"])/g,replaceFn) + '"';
-	return '"' + this + '"';
-};
-
 // Parse a space-separated string of name:value parameters
 //# where:
 //#   - the name or the value can be optional (in which case separate defaults are used instead)
