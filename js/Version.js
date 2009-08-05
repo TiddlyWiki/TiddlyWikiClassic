e90eae655611b6f8f40a76335dc18e363a6c54d8
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-08-05 12:59:10 +0000

    increased version to 2.5.3 alpha 2

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 5, revision: 3, alpha: 1, date: new Date("June 24, 2009"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 5, revision: 3, alpha: 2, date: new Date("Aug 5, 2009"), extensions: {}};
