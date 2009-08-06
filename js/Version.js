167adbc79879851803b83978b3c6e271cde61ef6
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-08-06 09:44:39 +0000

    beta: preparation for 2.5.3 beta 1

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 5, revision: 3, alpha: 2, date: new Date("Aug 5, 2009"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 5, revision: 3, beta: 1, date: new Date("Aug 6, 2009"), extensions: {}};
