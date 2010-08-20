95a6a5c7be2504555eff8be721671cbf13bc36e6
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-08-20 14:20:32 +0000

    post-release version increment

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 6, revision: 1, date: new Date("Aug 18, 2010"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 6, revision: 2, alpha: 1, date: new Date("Aug 19, 2010"), extensions: {}};
