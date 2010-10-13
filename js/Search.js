99d0014d793d9a1781da3b1a43dfb75c32a836d0
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-10-13 14:36:40 +0000

    prevented TAB from triggering incremental search
    Previously tabbing through the search field caused a search to be
    executed if the search box had a default value and incremental search
    was active. This is unexpected and thus confusing.
    
    cf. ticket #1262

--- js/Search.js
+++ js/Search.js
@@ -46,6 +46,8 @@ config.macros.search.onKeyPress = function(ev)
 	var me = config.macros.search;
 	var e = ev || window.event;
 	switch(e.keyCode) {
+		case 9: // Tab
+			return;
 		case 13: // Ctrl-Enter
 		case 10: // Ctrl-Enter on IE PC
 			me.doSearch(this);
