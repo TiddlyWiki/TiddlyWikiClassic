1a5a3b26d7ce05f37bf23fa2ce3c7598ecce76c5
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-11-04 17:18:19 +0000

    core: added extensible filter functions for Ticket #1265

--- js/Filters.js
+++ js/Filters.js
@@ -2,53 +2,62 @@
 //-- Filter a list of tiddlers
 //--
 
-// Filter a list of tiddlers
-//#   filter - filter expression (eg "tidlertitle [[multi word tiddler title]] [tag[systemConfig]]")
-//# Returns an array of Tiddler() objects that match the filter expression
-TiddlyWiki.prototype.filterTiddlers = function(filter)
-{
-	var results = [];
-	if(filter) {
-		var tiddler;
-		var re = /([^\s\[\]]+)|(?:\[([ \w]+)\[([^\]]+)\]\])|(?:\[\[([^\]]+)\]\])/mg;
-		var match = re.exec(filter);
-		while(match) {
-			if(match[1] || match[4]) {
-				//# matches (eg) text or [[tiddler title]]
-				var title = match[1] || match[4];
-				tiddler = this.fetchTiddler(title);
-				if(tiddler) {
-					results.pushUnique(tiddler);
-				} else if(this.isShadowTiddler(title)) {
-					tiddler = new Tiddler();
-					tiddler.set(title,this.getTiddlerText(title));
-					results.pushUnique(tiddler);
-				} else {
-					results.pushUnique(new Tiddler(title));
-				}
-			} else if(match[2]) {
-				//# matches (eg) [text[more text]]
-				switch(match[2]) {
-					case "tag":
-						var matched = this.getTaggedTiddlers(match[3]);
-						for(var m = 0; m < matched.length; m++)
-							results.pushUnique(matched[m]);
-						break;
-					case "sort":
-						results = this.sortTiddlers(results,match[3]);
-						break;
-					case "limit":
-						results = results.slice(0, parseInt(match[3],10));
-						break;
-					default:
-						var matched = this.getValueTiddlers(match[2],match[3]);
-						for(var m = 0; m < matched.length; m++)
-							results.pushUnique(matched[m]);
-						break;
-				}
+(function(){
+	//# text or [[tiddler title]] or [foo[bar]]
+	var re = /([^\s\[\]]+)|(?:\[([ \w]+)\[([^\]]+)\]\])|(?:\[\[([^\]]+)\]\])/mg;
+
+	//# Extensible filter functions
+	config.filters = {
+		tiddler: function(results,match) {
+			var title = match[1]||match[4];
+			var tiddler = this.fetchTiddler(title);
+			if(tiddler) {
+				results.pushUnique(tiddler);
+			} else if(this.isShadowTiddler(title)) {
+				tiddler = new Tiddler();
+				tiddler.set(title,this.getTiddlerText(title));
+				results.pushUnique(tiddler);
+			} else {
+				results.pushUnique(new Tiddler(title));
 			}
-			match = re.exec(filter);
+			return results;
+		},
+		tag: function(results,match) {
+			var matched = this.getTaggedTiddlers(match[3]);
+			for(var m=0; m<matched.length; m++) {
+				results.pushUnique(matched[m]);
+			}
+			return results;
+		},
+		sort: function(results,match) {
+			return this.sortTiddlers(results,match[3]);
+		},
+		limit: function(results,match) {
+			return results.slice(0,parseInt(match[3],10));
+		},
+		field: function(results,match) {
+			var matched = this.getValueTiddlers(match[2],match[3]);
+			for (var m = 0; m < matched.length; m++) {
+				results.pushUnique(matched[m]);
+			}
+			return results;
 		}
-	}
-	return results;
-};
+	};
+
+	// Filter a list of tiddlers
+	//#   filter - filter expression (eg "tidlertitle [[multi word tiddler title]] [tag[systemConfig]]")
+	//# Returns an array of Tiddler() objects that match the filter expression
+	TiddlyWiki.prototype.filterTiddlers = function(filter)
+	{
+		var results = [];
+		if(filter) {
+			var match = re.exec(filter);
+			while(match) {
+				var handler = (match[1]||match[4])?'tiddler':config.filters[match[2]]?match[2]:'field';
+				results = config.filters[handler].call(this,results,match);
+				match = re.exec(filter);
+			}
+		}
+		return results;
+	};
+}());
