1ecd4553351727a03505c93a04ba03a76c9c73ac
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-11-04 15:42:46 +0000

    core: refactored Filters.js into separate module ahead ofTicket #1265

--- js/TiddlyWiki.js
+++ js/TiddlyWiki.js
@@ -595,57 +595,6 @@ TiddlyWiki.prototype.resolveTiddler = function(tiddler)
 	return t instanceof Tiddler ? t : null;
 };
 
-// Filter a list of tiddlers
-//#   filter - filter expression (eg "tidlertitle [[multi word tiddler title]] [tag[systemConfig]]")
-//# Returns an array of Tiddler() objects that match the filter expression
-TiddlyWiki.prototype.filterTiddlers = function(filter)
-{
-	var results = [];
-	if(filter) {
-		var tiddler;
-		var re = /([^\s\[\]]+)|(?:\[([ \w]+)\[([^\]]+)\]\])|(?:\[\[([^\]]+)\]\])/mg;
-		var match = re.exec(filter);
-		while(match) {
-			if(match[1] || match[4]) {
-				//# matches (eg) text or [[tiddler title]]
-				var title = match[1] || match[4];
-				tiddler = this.fetchTiddler(title);
-				if(tiddler) {
-					results.pushUnique(tiddler);
-				} else if(this.isShadowTiddler(title)) {
-					tiddler = new Tiddler();
-					tiddler.set(title,this.getTiddlerText(title));
-					results.pushUnique(tiddler);
-				} else {
-					results.pushUnique(new Tiddler(title));
-				}
-			} else if(match[2]) {
-				//# matches (eg) [text[more text]]
-				switch(match[2]) {
-					case "tag":
-						var matched = this.getTaggedTiddlers(match[3]);
-						for(var m = 0; m < matched.length; m++)
-							results.pushUnique(matched[m]);
-						break;
-					case "sort":
-						results = this.sortTiddlers(results,match[3]);
-						break;
-					case "limit":
-						results = results.slice(0, parseInt(match[3],10));
-						break;
-					default:
-						var matched = this.getValueTiddlers(match[2],match[3]);
-						for(var m = 0; m < matched.length; m++)
-							results.pushUnique(matched[m]);
-						break;
-				}
-			}
-			match = re.exec(filter);
-		}
-	}
-	return results;
-};
-
 // Sort a list of tiddlers
 //# tiddlers - array of Tiddler() objects to be sorted
 //# field - name of field (or extended field) to sort by;
