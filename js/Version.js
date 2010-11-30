e99b3c38f29b865fd3ffa33a5ee09988f7772eb8
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-11-30 13:42:46 +0000

    TiddlyWiki beta - setting date.

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 6, revision: 2, beta: 4, date: new Date("Nov 23, 2010"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 6, revision: 2, beta: 4, date: new Date("Nov 30, 2010"), extensions: {}};
