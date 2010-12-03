2fb8852310d6ea40866df4bbbb5eb150236dc60e
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-12-03 11:37:50 +0000

    simplified saveTiddler overloading
    this also improves readability somewhat by highlighting how variables
    are swapped due to the overloading

--- js/TiddlyWiki.js
+++ js/TiddlyWiki.js
@@ -327,8 +327,8 @@ TiddlyWiki.prototype.saveTiddler = function(title,newTitle,newBody,modifier,modi
 	var tiddler;
 	if(title instanceof Tiddler) {
 		tiddler = title;
-		newTitle = tiddler.title;
 		title = tiddler.title;
+		newTitle = title;
 	} else {
 		tiddler = this.fetchTiddler(title);
 		if(tiddler) {
