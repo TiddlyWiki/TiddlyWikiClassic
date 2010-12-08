0f40cbf5d5047bbcda2348cdb7c2975ca051379b
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-12-08 12:58:26 +0000

    TiddlyWiki - preparation for 2.6.2 beta 7.

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 6, revision: 2, date: new Date("Dec 7, 2010"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 6, revision: 2, beta: 7, date: new Date("Dec 8, 2010"), extensions: {}};
