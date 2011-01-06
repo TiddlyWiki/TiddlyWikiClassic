16f292d8c6351a9b331ba516689acb75e815ed80
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2011-01-06 15:55:11 +0000

    TiddlyWiki - post release version number update

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 6, revision: 2, date: new Date("Jan 6, 2011"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 6, revision: 3, alpha: 1, date: new Date("Jan 6, 2011"), extensions: {}};
