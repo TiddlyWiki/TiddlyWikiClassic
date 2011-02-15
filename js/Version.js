047a9da7ff75fb7268e7414b9ba6fbc502271758
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2011-02-15 11:28:08 +0000

    TiddlyWiki - setting alpha 2 version

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 6, revision: 3, alpha: 1, date: new Date("Jan 6, 2011"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 6, revision: 3, alpha: 2, date: new Date("Feb 15, 2011"), extensions: {}};
