a72cbae84c817156e86fea259a13c62c77e894b8
Jon Robson <jdlrobson@gmail.com>
2011-03-18 16:01:44 +0000

    added additional test cases
    fields must have lowercase names, however getValue supports passing
    uppercase characters.
    
    However a slice with uppercase letters in it is valid
    
    These two tests will make sure these conditions are met.

--- test/js/TiddlerFields.js
+++ test/js/TiddlerFields.js
@@ -9,11 +9,15 @@ jQuery(document).ready(function(){
 		var val4 = store.getValue("testTiddler2","::slice");
 		var val5 = store.getValue("testTiddler2","##section");
 		var val6 = store.getValue("testTiddler2","##section2");
+		var val7 = store.getValue("testTiddler2","##Section3");
+		var val8 = store.getValue("testTiddler2","FieldValue");
 		strictEqual(val, "two");
 		strictEqual(val2, "199512010340");
 		strictEqual(val3, "testTag twoTag");
 		strictEqual(val4, "4t");
 		strictEqual(val5, "bar\n");
 		strictEqual(val6, "test\n");
+		strictEqual(val7, "welcome\n");
+		strictEqual(val8, "two");
 	});
 });
