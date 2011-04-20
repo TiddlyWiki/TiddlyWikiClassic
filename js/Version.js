ca072166a0d23a978cab9d12ceb81bf5603124a3
Chris Dent <chris.dent@gmail.com>
2011-04-20 18:06:54 +0100

    Make the current alpha be 2.6.3.A3
    js/Version.js leaves a second place where current version is
    established. The creation of ALPHA cut things down from 3 to 2.
    
    There's more of this kind of thing that can be done to make the
    release process smoother.

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 6, revision: 3, alpha: 2, date: new Date("Feb 15, 2011"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 6, revision: 3, alpha: 3, date: new Date("Apr 20, 2011"), extensions: {}};
