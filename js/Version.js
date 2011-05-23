087d6a1bb50e3d6ed49ab141e4e16a264c14df78
Chris Dent <chris.dent@gmail.com>
2011-05-23 13:22:51 +0100

    release 2.6.3 alpha 6
    * removed sortField parameter from getMissingLinks
    * #14: do not dirty the story when invoking loadMissinTiddler on non-file

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 6, revision: 3, alpha: 5, date: new Date("Apr 27, 2011"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 6, revision: 3, alpha: 6, date: new Date("May 23, 2011"), extensions: {}};
