61e0cc0f20467412de83de624c1b572d55f1f121
Martin Budden <mjbudden@gmail.com>
2011-05-26 08:11:40 +0100

    Corrected a RGB test. Added new RGB tests.

--- test/js/RGB.js
+++ test/js/RGB.js
@@ -5,13 +5,27 @@ jQuery(document).ready(function(){
 	test("RGB tests", function() {
 		expect(4);
 
-		var actual = new RGB(255,0,255).toString();
+		var actual = new RGB(1,0,1).toString();
 		var expected = "#ff00ff";
-		ok(actual==expected,'rgb(255,0,255) is the same as #ff00ff');
+		ok(actual==expected,'RGB(1,0,1) is the same as #ff00ff');
 
 		actual = new RGB("#f00").toString();
 		expected = "#ff0000";
 		ok(actual==expected,'#ff0000 is the same as #f00');
+
+		actual = new RGB("#123").toString();
+		console.log("actual",actual);
+		expected = "#112233";
+		ok(actual==expected,'#112233 is the same as #123');
+
+		actual = new RGB("#abc").toString();
+		console.log("actual",actual);
+		expected = "#aabbcc";
+		ok(actual==expected,'#aabbcc is the same as #abc');
+
+		actual = new RGB("#123456").toString();
+		expected = "#123456";
+		ok(actual==expected,'#123456 is the same as #123456');
 
 		actual = new RGB("rgb(0,255,0)").toString();
 		expected = "#00ff00";
@@ -22,7 +36,6 @@ jQuery(document).ready(function(){
 		//120 + (0 - 120) *0.5 and 0 + (255-0) * 0.5
 		expected = new RGB("rgb(60,127,0)").toString();
 		ok(actual==expected,'RGB mix function proportion 0.5');
-
 
 	});
 
