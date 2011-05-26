52678d466bc773c85ecad6148b3426f96f2a0855
Martin Budden <mjbudden@gmail.com>
2011-05-26 15:48:50 +0100

    Moved navigator dependent code out of Config.js into a separate module. Helps with https://github.com/TiddlyWiki/tiddlywiki/issues/22.

--- js/Config.js
+++ js/Config.js
@@ -154,22 +154,6 @@ config.commands = {
 	fields: {type: "popup"}
 };
 
-// Browser detection... In a very few places, there's nothing else for it but to know what browser we're using.
-config.userAgent = navigator.userAgent.toLowerCase();
-config.browser = {
-	isIE: config.userAgent.indexOf("msie") != -1 && config.userAgent.indexOf("opera") == -1,
-	isGecko: navigator.product == "Gecko" && config.userAgent.indexOf("WebKit") == -1,
-	ieVersion: /MSIE (\d.\d)/i.exec(config.userAgent), // config.browser.ieVersion[1], if it exists, will be the IE version string, eg "6.0"
-	isSafari: config.userAgent.indexOf("applewebkit") != -1,
-	isBadSafari: !((new RegExp("[\u0150\u0170]","g")).test("\u0150")),
-	firefoxDate: /gecko\/(\d{8})/i.exec(config.userAgent), // config.browser.firefoxDate[1], if it exists, will be Firefox release date as "YYYYMMDD"
-	isOpera: config.userAgent.indexOf("opera") != -1,
-	isLinux: config.userAgent.indexOf("linux") != -1,
-	isUnix: config.userAgent.indexOf("x11") != -1,
-	isMac: config.userAgent.indexOf("mac") != -1,
-	isWindows: config.userAgent.indexOf("win") != -1
-};
-
 // Control of macro parameter evaluation
 config.evaluateMacroParameters = "all";
 
@@ -180,7 +164,7 @@ config.textPrimitives = {
 	anyLetter:   "[A-Za-z0-9_\\-\u00c0-\u00de\u00df-\u00ff\u0150\u0170\u0151\u0171]",
 	anyLetterStrict: "[A-Za-z0-9\u00c0-\u00de\u00df-\u00ff\u0150\u0170\u0151\u0171]"
 };
-if(config.browser.isBadSafari) {
+if(!((new RegExp("[\u0150\u0170]","g")).test("\u0150"))) {
 	config.textPrimitives = {
 		upperLetter: "[A-Z\u00c0-\u00de]",
 		lowerLetter: "[a-z0-9_\\-\u00df-\u00ff]",
@@ -213,16 +197,12 @@ config.textPrimitives.tiddlerAnyLinkRegExp = new RegExp("("+ config.textPrimitiv
 	config.textPrimitives.urlPattern + ")","mg");
 
 config.glyphs = {
-	browsers: [
-		function() {return config.browser.isIE;},
-		function() {return true;}
-	],
-	currBrowser: null,
+	currBrowser: 0,
 	codes: {
-		downTriangle: ["\u25BC","\u25BE"],
-		downArrow: ["\u2193","\u2193"],
-		bentArrowLeft: ["\u2190","\u21A9"],
-		bentArrowRight: ["\u2192","\u21AA"]
+		downTriangle: ["\u25BE"],
+		downArrow: ["\u2193"],
+		bentArrowLeft: ["\u21A9"],
+		bentArrowRight: ["\u21AA"]
 	}
 };
 
