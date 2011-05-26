cc80dd05517c880644c77ef99d27cb05b127b463
Martin Budden <mjbudden@gmail.com>
2011-05-26 08:45:53 +0100

    Split augmented Number functions into separate module.

--- js/BasicTypes.js
+++ js/BasicTypes.js
@@ -1,19 +1,4 @@
 //--
-//-- Augmented methods for the JavaScript Number() object
-//--
-
-// Clamp a number to a range
-Number.prototype.clamp = function(min,max)
-{
-	var c = this;
-	if(c < min)
-		c = min;
-	if(c > max)
-		c = max;
-	return Number(c);
-};
-
-//--
 //-- Augmented methods for the JavaScript Array() object
 //--
 
