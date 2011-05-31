27efaaad60eccfd28dea81681a9068d5790fd897
Jon Robson <jdlrobson@gmail.com>
2011-05-31 11:46:35 +0100

    move version macro into Version.js
        The version macro is not entirely useful outside the context of
        standalone TiddlyWiki.
    
        It seems to me it would be more logically placed in Version.js

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,6 @@
 var version = {title: "TiddlyWiki", major: 2, minor: 6, revision: 3, alpha: 7, date: new Date("May 23, 2011"), extensions: {}};
+
+config.macros.version.handler = function(place)
+{
+	jQuery("<span/>").text(formatVersion()).appendTo(place);
+};
