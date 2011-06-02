1eeed1bce4472a414fd4096ec7a55b568078e0c1
Martin Budden <mjbudden@gmail.com>
2011-06-02 18:26:16 +0100

    Fixed erroneous comment.

--- deprecated/Utilities.js
+++ deprecated/Utilities.js
@@ -1,6 +1,6 @@
 //--
 //-- Deprecated utility functions
-//-- Use the jQuery ajax functions directly instead
+//-- Use the jQuery functions directly instead
 //--
 
 // Return the content of an element as plain text with no formatting
