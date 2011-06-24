9fb2020b94b9717980245d5ab1805dc24e9df73f
Martin Budden <mjbudden@gmail.com>
2011-06-24 12:37:36 +0100

    Reordered Utilities.js functions so that they are defined before they are used, as suggested by JSLint.

--- js/Utilities.js
+++ js/Utilities.js
@@ -34,6 +34,78 @@ function compareVersions(v1,v2)
 	return x1 > x2 ? -1 : 0;
 }
 
+function addClass(e,className)
+{
+	jQuery(e).addClass(className);
+}
+
+function removeClass(e,className)
+{
+	jQuery(e).removeClass(className);
+}
+
+function hasClass(e,className)
+{
+	return jQuery(e).hasClass(className);
+}
+
+function merge(dst,src,preserveExisting)
+{
+	var i;
+	for(i in src) {
+		if(!preserveExisting || dst[i] === undefined)
+			dst[i] = src[i];
+	}
+	return dst;
+}
+
+// Resolve the target object of an event
+function resolveTarget(e)
+{
+	var obj;
+	if(e.target)
+		obj = e.target;
+	else if(e.srcElement)
+		obj = e.srcElement;
+	if(obj.nodeType == 3) // defeat Safari bug
+		obj = obj.parentNode;
+	return obj;
+}
+
+// Returns a string containing the description of an exception, optionally prepended by a message
+function exceptionText(e,message)
+{
+	var s = e.description || e.toString();
+	return message ? "%0:\n%1".format([message,s]) : s;
+}
+
+// Displays an alert of an exception description with optional message
+function showException(e,message)
+{
+	alert(exceptionText(e,message));
+}
+
+function alertAndThrow(m)
+{
+	alert(m);
+	throw(m);
+}
+
+function glyph(name)
+{
+	var g = config.glyphs;
+	var b = g.currBrowser;
+	if(b == null) {
+		b = 0;
+		while(b < g.browsers.length-1 && !g.browsers[b]())
+			b++;
+		g.currBrowser = b;
+	}
+	if(!g.codes[name])
+		return "";
+	return g.codes[name][b];
+}
+
 function createTiddlyText(parent,text)
 {
 	return parent.appendChild(document.createTextNode(text));
@@ -97,37 +169,23 @@ function createTiddlyButton(parent,text,tooltip,action,className,id,accessKey,at
 	return btn;
 }
 
-//# Create a link to a particular tiddler
+//# Create a link to an external resource
 //#   place - element where the link should be created
-//#   title - title of target tiddler
-//#   includeText - flag for whether to include the title as the text of the link
-//#   className - custom CSS class for the link
-//#   linkedFromTiddler - tiddler from which to inherit extended fields
-//#   noToggle - flag to force the link to open the target, even if chkToggleLinks is on
-function createTiddlyLink(place,title,includeText,className,isStatic,linkedFromTiddler,noToggle)
+//#   url - link target
+//#   label - link text (optional)
+function createExternalLink(place,url,label)
 {
-	var text = includeText ? title : null;
-	var i = getTiddlyLinkInfo(title,className);
-	var btn = isStatic ? createExternalLink(place,store.getTiddlerText("SiteUrl",null) + "#" + title) : createTiddlyButton(place,text,i.subTitle,onClickTiddlerLink,i.classes);
-	if(isStatic)
-		btn.className += ' ' + className;
-	btn.setAttribute("refresh","link");
-	btn.setAttribute("tiddlyLink",title);
-	if(noToggle)
-		btn.setAttribute("noToggle","true");
-	if(linkedFromTiddler) {
-		var fields = linkedFromTiddler.getInheritedFields();
-		if(fields)
-			btn.setAttribute("tiddlyFields",fields);
-	}
-	return btn;
-}
-
-function refreshTiddlyLink(e,title)
-{
-	var i = getTiddlyLinkInfo(title,e.className);
-	e.className = i.classes;
-	e.title = i.subTitle;
+	var link = document.createElement("a");
+	link.className = "externalLink";
+	link.href = url;
+	var f = config.messages.externalLinkTooltip;
+	link.title = f ? f.format([url]) : url;
+	if(config.options.chkOpenInNewWindow)
+		link.target = "_blank";
+	place.appendChild(link);
+	if(label)
+		createTiddlyText(link, label);
+	return link;
 }
 
 function getTiddlyLinkInfo(title,currClasses)
@@ -157,25 +215,6 @@ function getTiddlyLinkInfo(title,currClasses)
 	if(typeof config.annotations[title]=="string")
 		subTitle = config.annotations[title];
 	return {classes: classes.join(" "),subTitle: subTitle};
-}
-
-//# Create a link to an external resource
-//#   place - element where the link should be created
-//#   url - link target
-//#   label - link text (optional)
-function createExternalLink(place,url,label)
-{
-	var link = document.createElement("a");
-	link.className = "externalLink";
-	link.href = url;
-	var f = config.messages.externalLinkTooltip;
-	link.title = f ? f.format([url]) : url;
-	if(config.options.chkOpenInNewWindow)
-		link.target = "_blank";
-	place.appendChild(link);
-	if(label)
-		createTiddlyText(link, label);
-	return link;
 }
 
 // Event handler for clicking on a tiddly link
@@ -211,15 +250,49 @@ function onClickTiddlerLink(ev)
 	return false;
 }
 
-// Create a button for a tag with a popup listing all the tiddlers that it tags
-//# title and tooltip arguments are optional
-function createTagButton(place,tag,excludeTiddler,title,tooltip)
+//# Create a link to a particular tiddler
+//#   place - element where the link should be created
+//#   title - title of target tiddler
+//#   includeText - flag for whether to include the title as the text of the link
+//#   className - custom CSS class for the link
+//#   linkedFromTiddler - tiddler from which to inherit extended fields
+//#   noToggle - flag to force the link to open the target, even if chkToggleLinks is on
+function createTiddlyLink(place,title,includeText,className,isStatic,linkedFromTiddler,noToggle)
 {
-	var btn = createTiddlyButton(place,title||tag,(tooltip||config.views.wikified.tag.tooltip).format([tag]),onClickTag);
-	btn.setAttribute("tag",tag);
-	if(excludeTiddler)
-		btn.setAttribute("tiddler",excludeTiddler);
+	var text = includeText ? title : null;
+	var i = getTiddlyLinkInfo(title,className);
+	var btn = isStatic ? createExternalLink(place,store.getTiddlerText("SiteUrl",null) + "#" + title) : createTiddlyButton(place,text,i.subTitle,onClickTiddlerLink,i.classes);
+	if(isStatic)
+		btn.className += ' ' + className;
+	btn.setAttribute("refresh","link");
+	btn.setAttribute("tiddlyLink",title);
+	if(noToggle)
+		btn.setAttribute("noToggle","true");
+	if(linkedFromTiddler) {
+		var fields = linkedFromTiddler.getInheritedFields();
+		if(fields)
+			btn.setAttribute("tiddlyFields",fields);
+	}
 	return btn;
+}
+
+function refreshTiddlyLink(e,title)
+{
+	var i = getTiddlyLinkInfo(title,e.className);
+	e.className = i.classes;
+	e.title = i.subTitle;
+}
+
+// Event handler for 'open all' on a tiddler popup
+function onClickTagOpenAll(ev)
+{
+	var tiddlers = store.getTaggedTiddlers(this.getAttribute("tag"));
+	var sortby = this.getAttribute("sortby");
+	if(sortby&&sortby.length) {
+		store.sortTiddlers(tiddlers,sortby);
+	}
+	story.displayTiddlers(this,tiddlers);
+	return false;
 }
 
 // Event handler for clicking on a tiddler tag
@@ -264,30 +337,15 @@ function onClickTag(ev)
 	return false;
 }
 
-// Event handler for 'open all' on a tiddler popup
-function onClickTagOpenAll(ev)
+// Create a button for a tag with a popup listing all the tiddlers that it tags
+//# title and tooltip arguments are optional
+function createTagButton(place,tag,excludeTiddler,title,tooltip)
 {
-	var tiddlers = store.getTaggedTiddlers(this.getAttribute("tag"));
-	var sortby = this.getAttribute("sortby");
-	if(sortby&&sortby.length) {
-		store.sortTiddlers(tiddlers,sortby);
-	}
-	story.displayTiddlers(this,tiddlers);
-	return false;
-}
-
-function onClickError(ev)
-{
-	var e = ev || window.event;
-	var popup = Popup.create(this);
-	var lines = this.getAttribute("errorText").split("\n");
-	var t;
-	for(t=0; t<lines.length; t++)
-		createTiddlyElement(popup,"li",null,null,lines[t]);
-	Popup.show();
-	e.cancelBubble = true;
-	if(e.stopPropagation) e.stopPropagation();
-	return false;
+	var btn = createTiddlyButton(place,title||tag,(tooltip||config.views.wikified.tag.tooltip).format([tag]),onClickTag);
+	btn.setAttribute("tag",tag);
+	if(excludeTiddler)
+		btn.setAttribute("tiddler",excludeTiddler);
+	return btn;
 }
 
 function createTiddlyDropDown(place,onchange,options,defaultValue)
@@ -304,17 +362,6 @@ function createTiddlyDropDown(place,onchange,options,defaultValue)
 	return sel;
 }
 
-function createTiddlyPopup(place,caption,tooltip,tiddler)
-{
-	if(tiddler.text) {
-		createTiddlyLink(place,caption,true);
-		var btn = createTiddlyButton(place,glyph("downArrow"),tooltip,onClickTiddlyPopup,"tiddlerPopupButton");
-		btn.tiddler = tiddler;
-	} else {
-		createTiddlyText(place,caption);
-	}
-}
-
 function onClickTiddlyPopup(ev)
 {
 	var e = ev || window.event;
@@ -329,69 +376,35 @@ function onClickTiddlyPopup(ev)
 	return false;
 }
 
+function createTiddlyPopup(place,caption,tooltip,tiddler)
+{
+	if(tiddler.text) {
+		createTiddlyLink(place,caption,true);
+		var btn = createTiddlyButton(place,glyph("downArrow"),tooltip,onClickTiddlyPopup,"tiddlerPopupButton");
+		btn.tiddler = tiddler;
+	} else {
+		createTiddlyText(place,caption);
+	}
+}
+
+function onClickError(ev)
+{
+	var e = ev || window.event;
+	var popup = Popup.create(this);
+	var lines = this.getAttribute("errorText").split("\n");
+	var t;
+	for(t=0; t<lines.length; t++)
+		createTiddlyElement(popup,"li",null,null,lines[t]);
+	Popup.show();
+	e.cancelBubble = true;
+	if(e.stopPropagation) e.stopPropagation();
+	return false;
+}
+
 function createTiddlyError(place,title,text)
 {
 	var btn = createTiddlyButton(place,title,null,onClickError,"errorButton");
 	if(text) btn.setAttribute("errorText",text);
-}
-
-function addClass(e,className)
-{
-	jQuery(e).addClass(className);
-}
-
-function removeClass(e,className)
-{
-	jQuery(e).removeClass(className);
-}
-
-function hasClass(e,className)
-{
-	return jQuery(e).hasClass(className);
-}
-
-function merge(dst,src,preserveExisting)
-{
-	var i;
-	for(i in src) {
-		if(!preserveExisting || dst[i] === undefined)
-			dst[i] = src[i];
-	}
-	return dst;
-}
-
-// Returns a string containing the description of an exception, optionally prepended by a message
-function exceptionText(e,message)
-{
-	var s = e.description || e.toString();
-	return message ? "%0:\n%1".format([message,s]) : s;
-}
-
-// Displays an alert of an exception description with optional message
-function showException(e,message)
-{
-	alert(exceptionText(e,message));
-}
-
-function alertAndThrow(m)
-{
-	alert(m);
-	throw(m);
-}
-
-function glyph(name)
-{
-	var g = config.glyphs;
-	var b = g.currBrowser;
-	if(b == null) {
-		b = 0;
-		while(b < g.browsers.length-1 && !g.browsers[b]())
-			b++;
-		g.currBrowser = b;
-	}
-	if(!g.codes[name])
-		return "";
-	return g.codes[name][b];
 }
 
 if(!window || !window.console) {
