f2d0c77831a27ad951a396ed6595d9221d11191a
Jon Robson <jdlrobson@gmail.com>
2011-06-28 12:14:46 +0100

    add tests for getValue / setValue methods on Wizard
    investigating a bug described here:
    https://github.com/TiddlySpace/tiddlyspace/issues/675

--- test/js/Wizard.js
+++ test/js/Wizard.js
@@ -11,5 +11,35 @@ jQuery(document).ready(function(){
 
 	});
 
+	test("Wizard: setValue / getValue (no formEl)", function() {
+		var w = new Wizard();
+		var val1 = w.getValue("test");
+		w.setValue("test", "foo");
+		var val2 = w.getValue("test");
+		strictEqual(val1, null, "no value set");
+		strictEqual(val2, null, "value could not be set as no formEl");
+	});
+
+	test("Wizard: setValue / getValue (formEl)", function() {
+		var w = new Wizard();
+		w.createWizard($("<div />")[0], "My Title");
+		var val1 = w.getValue("test1");
+		var elem = $("<div />").addClass("foo")[0];
+		w.setValue("test1", "foo");
+		w.setValue("test2", ["a list", "of items"]);
+		w.setValue("test3", { name: "data", val: "foo" });
+		w.setValue("test4", elem);
+		var val2 = w.getValue("test1");
+		var val3 = w.getValue("test2");
+		var val4 = w.getValue("test3");
+		var val5 = w.getValue("test4");
+
+		strictEqual(val1, undefined, "no value set but returns undefined if formEl exists");
+		strictEqual(val2, "foo", "value should be set in this situation");
+		strictEqual(val3.length, 2, "array set successfully (1/2)");
+		strictEqual(val3[0], "a list", "array set successfully (2/2)");
+		strictEqual(val4.name, "data", "object set successfully");
+		strictEqual($(val5).hasClass("foo"), true, "element set successfully");
+	});
 
 });
