f392826517e96ef369804feab3ba5c838c5d0c50
Martin Budden <mjbudden@gmail.com>
2011-07-21 14:45:06 +0100

    Version changed to 2.6.3 B1 for jQuery upgrade.

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 6, revision: 3, beta: 1, date: new Date("July 19, 2011"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 6, revision: 3, beta: 2, date: new Date("July 21, 2011"), extensions: {}};
