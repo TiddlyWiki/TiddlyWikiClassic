ffc230aae47d0ac82879c230886f3479791f7745
Martin Budden <mjbudden@gmail.com>
2011-07-29 14:03:11 +0100

    Changed version to 2.6.3 beta 3.

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 6, revision: 3, date: new Date("July 26, 2011"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 6, revision: 3, beta: 3, date: new Date("July 29, 2011"), extensions: {}};
