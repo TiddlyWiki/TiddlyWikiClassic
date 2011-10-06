9a7e190925503c1495edc10b7132853a80c86173
Martin Budden <mjbudden@gmail.com>
2011-10-06 16:04:38 +0100

    Preparation for 2.6.5 release

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 6, revision: 4, date: new Date("August 18, 2011"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 6, revision: 5, date: new Date("October 6, 2011"), extensions: {}};
