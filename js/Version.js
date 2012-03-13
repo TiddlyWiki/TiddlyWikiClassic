9b0d36b59390916d4e0bc6742da28c21fa6f0231
Jeremy Ruston <jeremy@osmosoft.com>
2012-03-13 17:38:03 +0000

    Advanced version number

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 6, revision: 5, date: new Date("October 6, 2011"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 6, revision: 6, alpha: 1, date: new Date("October 6, 2011"), extensions: {}};
