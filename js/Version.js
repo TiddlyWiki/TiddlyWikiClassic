d124a74a5ac889cd13d1a6d93162f420a7137689
Eric Shulman <elsdesign@gmail.com>
2012-09-15 21:26:14 -0700

    remove 'alpha' and update release date

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 6, revision: 6, alpha: 1, date: new Date("October 6, 2011"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 6, revision: 6, date: new Date("September 16, 2012"), extensions: {}};
