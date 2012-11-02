5c0d8317c0c9630e8f279e354dd577c1841a1833
Eric Shulman <elsdesign@gmail.com>
2012-11-01 22:08:20 -0700

    update version # for 270B2

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 7, revision: 0, beta: 1, date: new Date("October 22, 2012"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 7, revision: 0, beta: 2, date: new Date("November 1, 2012"), extensions: {}};
