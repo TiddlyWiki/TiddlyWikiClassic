ef9683e902ab7c63bcabf9c65ae9e28cf6730455
Eric Shulman <elsdesign@gmail.com>
2012-12-17 17:29:27 -0800

    TW2.7.0 release
    updated version # (removed beta)

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 7, revision: 0, beta: 2, date: new Date("November 1, 2012"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 7, revision: 0, date: new Date("December 15, 2012"), extensions: {}};
