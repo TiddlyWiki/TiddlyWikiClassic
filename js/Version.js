7b656ff39961ff0f1079f157b86e17aaf3046896
Eric Shulman <elsdesign@gmail.com>
2013-05-15 11:10:38 -0700

    update version/date for TW272 full release

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 7, revision: 2, beta: 2, date: new Date("May 13, 2013"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 7, revision: 2, date: new Date("May 15, 2013"), extensions: {}};
