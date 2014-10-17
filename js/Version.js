609b92dffe262b87af49ae35a4f7689294d48f8c
Eric Shulman <elsdesign@gmail.com>
2014-10-17 00:09:18 -0700

    update version info for TW290b2 release

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 9, revision: 0, beta: 1, date: new Date("August 23, 2014"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 9, revision: 0, beta: 2, date: new Date("October 15, 2014"), extensions: {}};
