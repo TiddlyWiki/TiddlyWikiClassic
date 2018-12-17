5c706d595d514e1eb9a0859fdb5595293c2576a5
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2018-12-17 18:00:56 +0300

    avoid empty query (?) or hash (#) in url after upgrading

--- js/Upgrade.js
+++ js/Upgrade.js
@@ -122,7 +122,7 @@ function changeUri(uri, modify)
 	var newScheme = parts.scheme === undefined ? '' : (parts.scheme + '//'),
 	    newHost   = parts.host || '',
 	    newPath   = parts.path || '',
-	    newQuery  = parts.query  === undefined ? '' : ('?' + parts.query),
+	    newQuery  = parts.query ? ('?' + parts.query) : '',
 	    newHash   = parts.hash   === undefined ? '' : ('#' + parts.hash);
 	return newScheme + newHost + newPath + newQuery + newHash;
 }
@@ -156,7 +156,7 @@ function stripUpgradePartsFromURI(uri)
 				hashParts = hashParts.slice(0,i);
 		
 		uriParts.query = queryParts.join('&');
-		uriParts.hash = hashParts.join('%20');
+		uriParts.hash = hashParts.join('%20') || undefined;
 	})
 }
 
