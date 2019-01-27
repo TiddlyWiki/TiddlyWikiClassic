b009e62866d495fb5acc237051da1b63385cd475
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2019-01-27 22:29:01 +0300

    substitute text of the close-messageArea button with icon, allow to disable (chkDontUseSvgIcons)

--- js/Messages.js
+++ js/Messages.js
@@ -7,12 +7,19 @@ function getMessageDiv()
 	var msgArea = document.getElementById("messageArea");
 	if(!msgArea)
 		return null;
-	if(!msgArea.hasChildNodes())
-		createTiddlyButton(createTiddlyElement(msgArea,"div",null,"messageArea__toolbar messageToolbar"),
+	if(!msgArea.hasChildNodes()) {
+		var btn = createTiddlyButton(createTiddlyElement(msgArea,"div",null,"messageArea__toolbar messageToolbar"),
 			config.messages.messageClose.text,
 			config.messages.messageClose.tooltip,
 			clearMessage,
 			"button messageToolbar__button");
+		if(!config.options.chkDontUseSvgIcons) {
+			btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10" class="messageToolbar__icon">'+
+			'	<rect width="1" height="13.1" x="4.5" y="-1.6" transform="rotate(-45 5 5)"/>'+
+			'	<rect width="1" height="13.1" x="4.5" y="-1.6" transform="rotate(+45 5 5)"/>'+
+			'</svg>';
+		}
+	}
 	msgArea.style.display = "block";
 	return createTiddlyElement(msgArea,"div",null,"messageArea__text");
 }
