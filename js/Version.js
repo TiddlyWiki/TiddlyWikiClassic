bfec8c01d8ebac54f9b716647d75261100de5e2c
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2019-02-23 16:59:27 +0300

    fix version detection for the next release

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = { title: "TiddlyWiki", major: 2, minor: 9, revision: 2, date: new Date("February 12, 2019"), extensions: {} };
+var version = {title: "TiddlyWiki", major: 2, minor: 9, revision: 2, date: new Date("February 12, 2019"), extensions: {}};
