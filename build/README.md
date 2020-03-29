e3d277a25542cc35472eeaeb609e512256902024
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2020-03-29 13:08:17 +0300

    build scripts: add generation of markup blocks and more in index.html, change the order of doc paragraphs

--- build/README.md
+++ build/README.md
@@ -19,14 +19,14 @@ Building is done in command line by simply running `npm run build-core`.
 
 Note that currently this doesn't include any auto-tests so be careful with the resulting core.
 
-Building index.html and generating RSS for classic.tiddlywiki.com
------------------------------------------------------------------
-With the same notes, building is done via `npm run build-site`. This also generates the RSS file
-(index.xml).
-
 Testing core via test.html
 --------------------------
 Currently, autotests are implemented in an old fashioned way: a new page (test.html) that is TW
 core with incorporated autotests is built via `npm run build-test`, than it should be opened
 in a browser and inspected visually. Note that load/save tests currently fail which is to be fixed
 in the future.
+
+Building index.html and generating RSS for classic.tiddlywiki.com
+-----------------------------------------------------------------
+With the same notes, building is done via `npm run build-site`. This also generates the RSS file
+(index.xml).
--- build/build_site.js
+++ build/build_site.js
@@ -1,5 +1,6 @@
-// this builds index.html (TW site contents) using index.html.recipe and build_settings.js
-// and generates RSS (index.xml) via TW's internal generateRss()
+// this builds index.html (TW site contents) using index.html.recipe and build_settings.js,
+// generates RSS (index.xml) via TW's internal generateRss()
+// and updates markup blocks etc of index.html using TW's internal updateOriginal()
 
 const joinPath = require('path').join;
 const exec = require('child_process').execSync;
@@ -26,10 +27,10 @@ const recipePath = joinPath(__dirname, 'index.html.recipe');
 let output = cookTwIntoFolder(recipePath, destinationPath, 'index.html');
 console.log(output);
 
-console.log(`BUILD: generating INDEX.XML (RSS feed)`);
+console.log(`BUILD: generating INDEX.XML (RSS feed) and static bits of INDEX.HTML`);
 process.env['TIDDLYWIKI_DESTINATION_PATH'] = destinationPath;
 // filenames (index.html and index.xml) are hardcoded inside generate_rss.js
-output = exec('phantomjs '+ joinPath(__dirname, 'generate_rss.js'));
+output = exec('phantomjs '+ joinPath(__dirname, 'generate_rss_and_static.js'));
 console.log(output.toString());
 
 console.log('BUILD: done');
