3f0bb6391cfddb0a7503e26695815b5e3a463276
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2020-04-05 20:46:57 +0300

    build scripts: introduce build tools and docs into the main repository
    introduces:
    
    * a minimal package.json (+ package-lock.json + .gitignore)
    * the `build/` directory with several build scripts and settings in build_settings.js
    * several npm commands for building (once `npm i` is run):
    ** `npm run build-core` builds empty TW
    ** `npm run build-external-core` builds empty TW with JS externalized into 3 files
    ** `npm run build-test` builds tests.html
    ** `npm run build-site` builds index.html and index.xml (RSS)
    * readme.md in the `build/` directory
    * dependencies for building: TW5, phantom.js

---
+++ build/build_core.js
@@ -0,0 +1,35 @@
+// this builds empty TW using tiddlywikinosaver.html.recipe and build_settings.js
+
+const joinPath = require('path').join;
+const fs = require('fs');
+const {
+	releaseVersion,
+	destinationRelativePath
+} = require('./build_settings.js');
+const {
+	makeSureFolderExists,
+	clearFolder,
+	cookTwIntoFolder,
+	fixupEmptyLinesInStoreArea
+} = require('./build_utils.js');
+
+const destinationPath = joinPath(__dirname, destinationRelativePath);
+makeSureFolderExists(destinationPath);
+
+console.log(`BUILD: clearing target folder: "${destinationRelativePath}"`);
+clearFolder(destinationPath);
+console.log();
+
+console.log(`BUILD: assembling EMPTY.HTML (v${releaseVersion})`);
+const recipePath = joinPath(__dirname, '../tiddlywikinosaver.html.recipe');
+let output = cookTwIntoFolder(recipePath, destinationPath, 'empty.html');
+console.log(output);
+
+console.log('BUILD: removing empty lines from storeArea of EMPTY.HTML');
+const resultPath = joinPath(destinationPath, 'empty.html');
+const result = fs.readFileSync(resultPath).toString();
+const fixedResult = fixupEmptyLinesInStoreArea(result);
+fs.writeFileSync(resultPath, fixedResult);
+console.log();
+
+console.log('BUILD: done');
---
+++ build/build_utils.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const joinPath = require('path').join;
+const exec = require('child_process').execSync;
+const {
+	releaseVersion, //# overwrite Version.js ?
+} = require('./build_settings.js');
+
+module.exports = {
+	makeSureFolderExists: function (path) {
+		if(!fs.existsSync(path))
+			fs.mkdirSync(path, {recursive: true});
+	},
+	clearFolder: function (path) {
+		const files = fs.readdirSync(path);
+		for(const file of files) {
+			fs.unlink(joinPath(path, file), err => {
+				if (err) throw err;
+			});
+		}
+	},
+	//# saving into folder should be separated from cooking instead (limitation of TW5)
+	cookTwIntoFolder: function (recipePath, destinationPath, resultFileName, customTw5Template) {
+		const tw5Command = 'tiddlywiki';
+		const tw5OptionsPath = joinPath(__dirname, './tw5CookOptions');
+		const resultPath = joinPath(destinationPath, resultFileName);
+		const tw5Template = customTw5Template || 'tiddlywiki2.template.html';
+		let output = exec(`${tw5Command} ${tw5OptionsPath} --verbose --load ${recipePath}` +
+			` --rendertiddler $:/core/templates/${tw5Template} ${resultPath} text/plain`);
+		return output.toString();
+	},
+	fixupEmptyLinesInStoreArea: function (htmlAsString) {
+		return htmlAsString.replace(/(<(div|DIV) (?:id|ID)=["']?storeArea['"]?>)(\n+)<\/\2>/,
+			(wholeThing, openTag, tagName) => `${openTag}</${tagName}>`)
+	},
+};
