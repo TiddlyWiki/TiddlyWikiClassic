1126412263098236a6ee11dbac73397e32a641fa
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2022-06-20 20:51:44 +0300

    codestyle: update loader/saver files

--- js/LoaderSaver.js
+++ js/LoaderSaver.js
@@ -2,45 +2,44 @@
 //-- LoaderBase and SaverBase
 //--
 
-//# LoaderBase: A (abstract) storage loader that loads the tiddlers from a list of HTML elements.
+//# LoaderBase: An (abstract) storage loader that loads the tiddlers from a list of HTML elements.
 //# The format of the elements is defined by subclasses of this loader through the internalizeTiddler implementation.
 //# Subclasses must implement:
-//#			function getTitle(store,node)
-//#			function internalizeTiddler(store,tiddler,title,node)
+//#			function getTitle(store, node)
+//#			function internalizeTiddler(store, tiddler, title, node)
 //#
 //# store must implement:
 //#			function createTiddler(title)
 function LoaderBase() {}
 
-LoaderBase.prototype.loadTiddler = function(store,node,tiddlers)
+LoaderBase.prototype.loadTiddler = function(store, node, tiddlers)
 {
-	var title = this.getTitle(store,node);
-	if(safeMode && store.isShadowTiddler(title))
-		return;
-	if(title) {
-		var tiddler = store.createTiddler(title);
-		this.internalizeTiddler(store,tiddler,title,node);
-		tiddlers.push(tiddler);
-	}
+	var title = this.getTitle(store, node);
+	if(!title) return;
+	if(safeMode && store.isShadowTiddler(title)) return;
+
+	var tiddler = store.createTiddler(title);
+	this.internalizeTiddler(store, tiddler, title, node);
+	tiddlers.push(tiddler);
 };
 
-LoaderBase.prototype.loadTiddlers = function(store,nodes)
+LoaderBase.prototype.loadTiddlers = function(store, nodes)
 {
-	var t,tiddlers = [];
-	for(t = 0; t < nodes.length; t++) {
+	var i, tiddlers = [];
+	for(i = 0; i < nodes.length; i++) {
 		try {
-			this.loadTiddler(store,nodes[t],tiddlers);
+			this.loadTiddler(store, nodes[i], tiddlers);
 		} catch(ex) {
-			showException(ex,config.messages.tiddlerLoadError.format([this.getTitle(store,nodes[t])]));
+			showException(ex, config.messages.tiddlerLoadError.format([this.getTitle(store, nodes[i])]));
 		}
 	}
 	return tiddlers;
 };
 
-//# SaverBase: a (abstract) storage saver that externalizes all tiddlers into a string,
+//# SaverBase: an (abstract) storage saver that externalizes all tiddlers into a string,
 //# with every tiddler individually externalized (using this.externalizeTiddler) and joined with newlines
 //# Subclasses must implement:
-//#			function externalizeTiddler(store,tiddler)
+//#			function externalizeTiddler(store, tiddler)
 //#
 //# store must implement:
 //#			function getTiddlers(sortByFieldName)
@@ -49,10 +48,10 @@ function SaverBase() {}
 SaverBase.prototype.externalize = function(store)
 {
 	var results = [];
-	var t,tiddlers = store.getTiddlers("title");
-	for(t = 0; t < tiddlers.length; t++) {
-		if(!tiddlers[t].doNotSave())
-			results.push(this.externalizeTiddler(store, tiddlers[t]));
+	var i, tiddlers = store.getTiddlers("title");
+	for(i = 0; i < tiddlers.length; i++) {
+		if(!tiddlers[i].doNotSave())
+			results.push(this.externalizeTiddler(store, tiddlers[i]));
 	}
 	return results.join("\n");
 };
--- js/TW21Loader.js
+++ js/TW21Loader.js
@@ -6,33 +6,31 @@ function TW21Loader() {}
 
 TW21Loader.prototype = new LoaderBase();
 
-TW21Loader.prototype.getTitle = function(store,node)
+TW21Loader.prototype.getTitle = function(store, node)
 {
 	var title = null;
 	if(node.getAttribute) {
-		title = node.getAttribute("title");
-		if(!title)
-			title = node.getAttribute("tiddler");
+		title = node.getAttribute("title") || node.getAttribute("tiddler");
 	}
 	if(!title && node.id) {
-		var lenPrefix = store.idPrefix.length;
-		if(node.id.substr(0,lenPrefix) == store.idPrefix)
-			title = node.id.substr(lenPrefix);
+		var prefixLen = store.idPrefix.length;
+		if(node.id.substr(0, prefixLen) == store.idPrefix)
+			title = node.id.substr(prefixLen);
 	}
 	return title;
 };
 
-TW21Loader.prototype.internalizeTiddler = function(store,tiddler,title,node)
+TW21Loader.prototype.internalizeTiddler = function(store, tiddler, title, node)
 {
 	var e = node.firstChild;
 	var text = null;
-	if(node.getAttribute("tiddler")) {
+	if(node.getAttribute && node.getAttribute("tiddler")) {
 		text = getNodeText(e).unescapeLineBreaks();
 	} else {
-		while(e.nodeName!="PRE" && e.nodeName!="pre") {
+		while(e.nodeName != "PRE" && e.nodeName != "pre") {
 			e = e.nextSibling;
 		}
-		text = e.innerHTML.replace(/\r/mg,"").htmlDecode();
+		text = e.innerHTML.replace(/\r/mg, "").htmlDecode();
 	}
 	var creator = node.getAttribute("creator");
 	var modifier = node.getAttribute("modifier");
@@ -42,14 +40,14 @@ TW21Loader.prototype.internalizeTiddler = function(store,tiddler,title,node)
 	var modified = m ? Date.convertFromYYYYMMDDHHMMSS(m) : created;
 	var tags = node.getAttribute("tags");
 	var fields = {};
-	var i,attrs = node.attributes;
-	for(i = attrs.length-1; i >= 0; i--) {
+	var i, attrs = node.attributes;
+	for(i = attrs.length - 1; i >= 0; i--) {
 		var name = attrs[i].name;
 		if(attrs[i].specified && !TiddlyWiki.isStandardField(name)) {
 			fields[name] = attrs[i].value.unescapeLineBreaks();
 		}
 	}
-	tiddler.assign(title,text,modifier,modified,tags,created,fields,creator);
+	tiddler.assign(title, text, modifier, modified, tags, created, fields, creator);
 	return tiddler;
 };
 
