99626fcf4f4c06126d611298da128ee2bff68efe
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2022-08-21 17:49:41 +0300

    codestyle: update FormatterHelpers.js

--- js/FormatterHelpers.js
+++ js/FormatterHelpers.js
@@ -4,21 +4,20 @@
 
 function Formatter(formatters)
 {
-	var n;
 	this.formatters = [];
 	var pattern = [];
-	for(n=0; n<formatters.length; n++) {
+	for(var n = 0; n < formatters.length; n++) {
 		pattern.push("(" + formatters[n].match + ")");
 		this.formatters.push(formatters[n]);
 	}
-	this.formatterRegExp = new RegExp(pattern.join("|"),"mg");
+	this.formatterRegExp = new RegExp(pattern.join("|"), "mg");
 }
 
 config.formatterHelpers = {
 
 	createElementAndWikify: function(w)
 	{
-		w.subWikifyTerm(createTiddlyElement(w.output,this.element),this.termRegExp);
+		w.subWikifyTerm(createTiddlyElement(w.output, this.element), this.termRegExp);
 	},
 
 	inlineCssHelper: function(w)
@@ -27,7 +26,7 @@ config.formatterHelpers = {
 		config.textPrimitives.cssLookaheadRegExp.lastIndex = w.nextMatch;
 		var lookaheadMatch = config.textPrimitives.cssLookaheadRegExp.exec(w.source);
 		while(lookaheadMatch && lookaheadMatch.index == w.nextMatch) {
-			var s,v;
+			var s, v;
 			if(lookaheadMatch[1]) {
 				s = lookaheadMatch[1].unDash();
 				v = lookaheadMatch[2];
@@ -35,11 +34,11 @@ config.formatterHelpers = {
 				s = lookaheadMatch[3].unDash();
 				v = lookaheadMatch[4];
 			}
-			if(s=="bgcolor")
+			if(s == "bgcolor")
 				s = "backgroundColor";
-			if(s=="float")
+			if(s == "float")
 				s = "cssFloat";
-			styles.push({style: s, value: v});
+			styles.push({ style: s, value: v });
 			w.nextMatch = lookaheadMatch.index + lookaheadMatch[0].length;
 			config.textPrimitives.cssLookaheadRegExp.lastIndex = w.nextMatch;
 			lookaheadMatch = config.textPrimitives.cssLookaheadRegExp.exec(w.source);
@@ -47,14 +46,12 @@ config.formatterHelpers = {
 		return styles;
 	},
 
-	applyCssHelper: function(e,styles)
+	applyCssHelper: function(e, styles)
 	{
-		var t;
-		for(t=0; t< styles.length; t++) {
+		for(var i = 0; i < styles.length; i++) {
 			try {
-				e.style[styles[t].style] = styles[t].value;
-			} catch (ex) {
-			}
+				e.style[styles[i].style] = styles[i].value;
+			} catch (ex) {}
 		}
 	},
 
@@ -65,8 +62,8 @@ config.formatterHelpers = {
 		if(lookaheadMatch && lookaheadMatch.index == w.matchStart) {
 			var text = lookaheadMatch[1];
 			if(config.browser.isIE && (config.browser.ieVersion[1] < 10))
-				text = text.replace(/\n/g,"\r");
-			createTiddlyElement(w.output,this.element,null,null,text);
+				text = text.replace(/\n/g, "\r");
+			createTiddlyElement(w.output, this.element, null, null, text);
 			w.nextMatch = lookaheadMatch.index + lookaheadMatch[0].length;
 		}
 	},
@@ -74,21 +71,22 @@ config.formatterHelpers = {
 	isExternalLink: function(link)
 	{
 		if(store.tiddlerExists(link) || store.isShadowTiddler(link)) {
-			//# Definitely not an external link
 			return false;
 		}
-		var urlRegExp = new RegExp(config.textPrimitives.urlPattern,"mg");
+		var urlRegExp = new RegExp(config.textPrimitives.urlPattern, "mg");
 		if(urlRegExp.exec(link)) {
-			//# Definitely an external link
 			return true;
 		}
-		if(link.indexOf(".")!=-1 || link.indexOf("\\")!=-1 || link.indexOf("/")!=-1 || link.indexOf("#")!=-1) {
-			//# Link contains . / \ or # so is probably an external link
+		//# May give false positives (a link to not-yet-existing tiddler with one of these chars in title)
+		if(link.indexOf(".") != -1 ||
+		   link.indexOf("\\") != -1 ||
+		   link.indexOf("/") != -1 ||
+		   link.indexOf("#") != -1
+		) {
 			return true;
 		}
-		//# Otherwise assume it is not an external link
+		//# Unexisting tiddler without . / \ or # in title
 		return false;
 	}
-
 };
 
