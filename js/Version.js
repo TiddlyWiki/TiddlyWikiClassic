68bbcb5eabb1d17774c3826070cd612b54974e7e
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2023-05-11 22:55:10 +0300

    bump version to 2.9.4

--- js/Version.js
+++ js/Version.js
@@ -1,1 +1,1 @@
-var version = {title: "TiddlyWiki", major: 2, minor: 9, revision: 3, date: new Date("June 27, 2022"), extensions: {}};
+var version = {title: "TiddlyWiki", major: 2, minor: 9, revision: 4, date: new Date("May 11, 2023"), extensions: {}};
